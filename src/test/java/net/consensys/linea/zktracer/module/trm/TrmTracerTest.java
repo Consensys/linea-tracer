8a751ba4a60f1788f91ee933891b344a38b51453
Sally MacFarlane <macfarla.github@gmail.com>
2023-04-26 10:09:23 +1000

    trm only has one arg
    Signed-off-by: Sally MacFarlane <macfarla.github@gmail.com>

--- src/test/java/net/consensys/linea/zktracer/module/trm/TrmTracerTest.java
+++ src/test/java/net/consensys/linea/zktracer/module/trm/TrmTracerTest.java
@@ -2,26 +2,39 @@ package net.consensys.linea.zktracer.module.trm;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 import java.util.stream.Stream;
 
+import net.consensys.linea.zktracer.AbstractModuleTracerCorsetTest;
 import net.consensys.linea.zktracer.OpCode;
-import net.consensys.linea.zktracer.module.AbstractModuleTracerTest;
 import net.consensys.linea.zktracer.module.ModuleTracer;
+import org.apache.tuweni.bytes.Bytes32;
 import org.apache.tuweni.units.bigints.UInt256;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.provider.Arguments;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 @ExtendWith(MockitoExtension.class)
-public class TrmTracerTest extends AbstractModuleTracerTest {
+public class TrmTracerTest extends AbstractModuleTracerCorsetTest {
+  private final Random rand = new Random();
+
   @Override
   protected Stream<Arguments> provideNonRandomArguments() {
     List<Arguments> arguments = new ArrayList<>();
     for (OpCode opCode : getModuleTracer().supportedOpCodes()) {
-      for (int k = 0; k <= 3; k++) {
-        for (int i = 1; i <= 4; i++) {
-          arguments.add(Arguments.of(opCode, UInt256.valueOf(i), UInt256.valueOf(k)));
-        }
+      for (int i = 1; i <= 4; i++) {
+        arguments.add(Arguments.of(opCode, List.of(UInt256.valueOf(i))));
+      }
+    }
+    return arguments.stream();
+  }
+
+  @Override
+  public Stream<Arguments> provideRandomArguments() {
+    final List<Arguments> arguments = new ArrayList<>();
+    for (OpCode opCode : getModuleTracer().supportedOpCodes()) {
+      for (int i = 0; i <= 16; i++) {
+        arguments.add(Arguments.of(opCode, List.of(Bytes32.random(rand))));
       }
     }
     return arguments.stream();

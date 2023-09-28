b10cf1f93fa7d60fe8f6b0f8243d697a3a8685f5
Francois Bojarski <francois.bojarski@consensys.net>
2023-09-28 19:48:13 +0530

    refactor: move bigIntToBytes

---
+++ src/main/java/net/consensys/linea/zktracer/bytes/conversions.java
@@ -0,0 +1,24 @@
+package net.consensys.linea.zktracer.bytes;
+
+import java.math.BigInteger;
+
+import org.apache.tuweni.bytes.Bytes;
+
+public class conversions {
+  public static Bytes bigIntegerToBytes(BigInteger big) {
+    Bytes bytes;
+    if (big.equals(BigInteger.ZERO)){
+      bytes = Bytes.of(0x00);
+    } else {
+      byte[] byteArray;
+      byteArray = big.toByteArray();
+      if (byteArray[0] == 0) {
+        Bytes tmp = Bytes.wrap(byteArray);
+        bytes = Bytes.wrap(tmp.slice(1, tmp.size() - 1));
+      } else {
+        bytes = Bytes.wrap(byteArray);
+      }
+    }
+    return bytes;
+  }
+}

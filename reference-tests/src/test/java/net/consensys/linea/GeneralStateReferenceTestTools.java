2a1ee56d419172ef8eced1c97a0688718f6e5059
Franklin Delehelle <franklin.delehelle@odena.eu>
2023-10-28 01:19:33 +0200

    test: only test on London

--- reference-tests/src/test/java/net/consensys/linea/GeneralStateReferenceTestTools.java
+++ reference-tests/src/test/java/net/consensys/linea/GeneralStateReferenceTestTools.java
@@ -70,7 +70,7 @@ public class GeneralStateReferenceTestTools {
               (testName, fullPath, stateSpec, collector) -> {
                 final String prefix = testName + "-";
                 for (final Map.Entry<String, List<GeneralStateTestCaseEipSpec>> entry :
-                    stateSpec.finalStateSpecs().entrySet()) {
+                    stateSpec.finalStateSpecs().entrySet().stream().filter(e -> e.getKey().equalsIgnoreCase("London")).toList()) {
                   final String eip = entry.getKey();
                   final boolean runTest = EIPS_TO_RUN.contains(eip);
                   final List<GeneralStateTestCaseEipSpec> eipSpecs = entry.getValue();

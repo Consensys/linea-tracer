e7e771df88adb5332a2f6d40d8407be5836c4b1c
Franklin Delehelle <franklin.delehelle@odena.eu>
2023-10-28 16:15:21 +0200

    Fix

--- arithmetization/src/main/java/net/consensys/linea/zktracer/module/rom/Rom.java
+++ arithmetization/src/main/java/net/consensys/linea/zktracer/module/rom/Rom.java
@@ -23,6 +23,7 @@ import net.consensys.linea.zktracer.module.Module;
 import net.consensys.linea.zktracer.module.ModuleTrace;
 import net.consensys.linea.zktracer.module.romLex.RomChunk;
 import net.consensys.linea.zktracer.module.romLex.RomLex;
+import net.consensys.linea.zktracer.types.UnsignedByte;
 import org.apache.tuweni.bytes.Bytes;
 
 public class Rom implements Module {

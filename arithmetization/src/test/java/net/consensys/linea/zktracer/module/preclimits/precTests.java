94ffe114bc08fc069a04997c18d47f1b1430c860
Sally MacFarlane <macfarla.github@gmail.com>
2023-11-08 12:31:08 +1000

    copyright
    Signed-off-by: Sally MacFarlane <macfarla.github@gmail.com>

--- arithmetization/src/test/java/net/consensys/linea/zktracer/module/preclimits/precTests.java
+++ arithmetization/src/test/java/net/consensys/linea/zktracer/module/preclimits/precTests.java
@@ -1,5 +1,5 @@
 package net.consensys.linea.zktracer.module.preclimits; /*
-                                                         * Copyright ConsenSys AG.
+                                                         * Copyright Consensys Software Inc.
                                                          *
                                                          * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
                                                          * the License. You may obtain a copy of the License at

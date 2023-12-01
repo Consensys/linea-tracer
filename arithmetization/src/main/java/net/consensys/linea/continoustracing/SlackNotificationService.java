6f41dad734d99577915fd9378c2729e6f071ba15
Franklin Delehelle <franklin.delehelle@odena.eu>
2023-12-01 09:21:29 +0100

    feat: try to read `TRACES_DIR` in the traces API

--- arithmetization/src/main/java/net/consensys/linea/continoustracing/SlackNotificationService.java
+++ arithmetization/src/main/java/net/consensys/linea/continoustracing/SlackNotificationService.java
@@ -65,12 +65,11 @@ public class SlackNotificationService {
                         section ->
                             section.text(
                                 markdownText(
-                                    "Block hash: *"
-                                        + blockHash
-                                        + "*\n\n"
-                                        + "Trace verification failed with the following error:\n\n"
+                                    "Trace verification failed with the following error:\n\n"
                                         + "```"
-                                        + validationResult.corsetOutput()
+                                        + validationResult
+                                            .corsetOutput()
+                                            .replaceAll("\u001B\\[[;\\d]*m", "")
                                         + "```\n\n"
                                         + "Trace file: "
                                         + validationResult.traceFile())))))

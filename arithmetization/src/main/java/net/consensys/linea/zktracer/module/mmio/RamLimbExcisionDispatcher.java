cdcbede279fa95e1d53008c206f82ba50da46c34
Tsvetan Dimitrov <tsvetan.dimitrov@consensys.net>
2023-12-05 16:04:39 +0200

    feat(mmio): add RamLimbExcisionDispatcher

---
+++ arithmetization/src/main/java/net/consensys/linea/zktracer/module/mmio/RamLimbExcisionDispatcher.java
@@ -0,0 +1,75 @@
+/*
+ * Copyright Consensys Software Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package net.consensys.linea.zktracer.module.mmio;
+
+import com.google.common.base.Preconditions;
+import net.consensys.linea.zktracer.module.mmu.MicroData;
+import net.consensys.linea.zktracer.runtime.callstack.CallStack;
+import net.consensys.linea.zktracer.types.UnsignedByte;
+
+public class RamLimbExcisionDispatcher implements MmioDispatcher {
+  @Override
+  public MmioData dispatch(MicroData microData, CallStack callStack) {
+    MmioData mmioData = new MmioData();
+    mmioData.cnA(0);
+    mmioData.cnB(microData.targetContext());
+    mmioData.cnC(0);
+    mmioData.indexA(0);
+    mmioData.indexB(microData.targetLimbOffset().toInt());
+    mmioData.indexC(0);
+    mmioData.valA(new UnsignedByte[16]);
+    mmioData.valB(callStack.valueFromMemory(mmioData.cnB(), mmioData.indexB()));
+    mmioData.valC(new UnsignedByte[16]);
+    mmioData.valANew(mmioData.valA());
+    mmioData.valBNew(
+        excise(mmioData.valB(), microData.targetByteOffset().toInteger(), microData.size()));
+    mmioData.valCNew(mmioData.valC());
+    mmioData.updateLimbsInMemory(callStack);
+
+    return mmioData;
+  }
+
+  private UnsignedByte[] excise(UnsignedByte[] source, int start, int size) {
+    int end = start + size;
+
+    Preconditions.checkState(
+        start < 0 || size <= 0,
+        "offsets out of bounds: start = %d, size = %d\none should have start >= 0 & size > 0"
+            .formatted(start, size));
+
+    // TODO: explore this case: it happens in practice : )
+    //    Preconditions.checkState(
+    //      end > 16,
+    //      "offsets out of bounds:, start + size = %d is bigger than 16".formatted(end));
+
+    for (int i = start; i < Math.min(end, 16); i++) {
+      source[i] = UnsignedByte.ZERO;
+    }
+
+    return source;
+  }
+
+  @Override
+  public void update(MmioData mmioData, MicroData microData, int counter) {
+    mmioData.excision(
+        mmioData.byteB(counter),
+        mmioData.acc1(),
+        mmioData.pow2561(),
+        microData.targetByteOffset(),
+        microData.size(),
+        counter);
+  }
+}

ebe3c3c13e3cfaee0a18e3ffcc1e77e718890b82
Franklin Delehelle <franklin.delehelle@odena.eu>
2023-12-09 17:00:31 +0100

    perf: delay AddOperation computation

--- arithmetization/src/main/java/net/consensys/linea/zktracer/module/add/AddOperation.java
+++ arithmetization/src/main/java/net/consensys/linea/zktracer/module/add/AddOperation.java
@@ -15,7 +15,128 @@
 
 package net.consensys.linea.zktracer.module.add;
 
+import com.google.common.base.Objects;
+import net.consensys.linea.zktracer.bytestheta.BaseBytes;
 import net.consensys.linea.zktracer.opcode.OpCode;
+import net.consensys.linea.zktracer.opcode.OpCodeData;
+import net.consensys.linea.zktracer.opcode.OpCodes;
+import net.consensys.linea.zktracer.types.Bytes16;
+import net.consensys.linea.zktracer.types.UnsignedByte;
+import org.apache.tuweni.bytes.Bytes;
 import org.apache.tuweni.bytes.Bytes32;
+import org.apache.tuweni.units.bigints.UInt256;
 
-public record AddOperation(OpCode opCodem, Bytes32 arg1, Bytes32 arg2) {}
+public final class AddOperation {
+  private static final UInt256 TWO_TO_THE_128 = UInt256.ONE.shiftLeft(128);
+
+  private final OpCode opCode;
+  private final Bytes rawArg1;
+  private final Bytes rawArg2;
+  private Bytes32 arg1;
+  private Bytes32 arg2;
+
+  /**
+   * Returns the appropriate state of the overflow bit depending on the position within the cycle.
+   *
+   * @param counter current position within the tracing cycle
+   * @param overflowHi putative overflow bit of the high part
+   * @param overflowLo putative overflow bit of the high part
+   * @return the overflow bit to trace
+   */
+  private static boolean overflowBit(
+      final int counter, final boolean overflowHi, final boolean overflowLo) {
+    if (counter == 14) {
+      return overflowHi;
+    }
+
+    if (counter == 15) {
+      return overflowLo;
+    }
+
+    return false;
+  }
+
+  public AddOperation(OpCode opCode, Bytes arg1, Bytes arg2) {
+    this.opCode = opCode;
+    this.rawArg1 = arg1;
+    this.rawArg2 = arg2;
+  }
+
+  void trace(int stamp, Trace trace) {
+    this.arg1 = Bytes32.leftPad(this.rawArg1);
+    this.arg2 = Bytes32.leftPad(this.rawArg2);
+
+    final Bytes16 arg1Hi = Bytes16.wrap(arg1.slice(0, 16));
+    final Bytes32 arg1Lo = Bytes32.leftPad(arg1.slice(16));
+    final Bytes16 arg2Hi = Bytes16.wrap(arg2.slice(0, 16));
+    final Bytes16 arg2Lo = Bytes16.wrap(arg2.slice(16));
+
+    boolean overflowHi = false;
+    boolean overflowLo;
+
+    final OpCodeData opCodeData = OpCodes.of(opCode);
+
+    final BaseBytes res = Adder.addSub(opCode, arg1, arg2);
+
+    final Bytes16 resHi = res.getHigh();
+    final Bytes16 resLo = res.getLow();
+
+    UInt256 arg1Int = UInt256.fromBytes(arg1);
+    UInt256 arg2Int = UInt256.fromBytes(arg2);
+    UInt256 resultBytes;
+
+    if (opCode == OpCode.ADD) {
+      resultBytes = arg1Int.add(arg2Int);
+      if (resultBytes.compareTo(arg1Int) < 0) {
+        overflowHi = true;
+      }
+    } else if (opCode == OpCode.SUB) {
+      if (arg1Int.compareTo(arg2Int) < 0) {
+        overflowHi = true;
+      }
+    }
+
+    for (int i = 0; i < 16; i++) {
+      Bytes32 addRes;
+      if (opCode == OpCode.ADD) {
+        addRes = Bytes32.wrap((UInt256.fromBytes(arg1Lo)).add(UInt256.fromBytes(arg2Lo)));
+      } else {
+        addRes = Bytes32.wrap((UInt256.fromBytes(resLo)).add(UInt256.fromBytes(arg2Lo)));
+      }
+
+      overflowLo = (addRes.compareTo(TWO_TO_THE_128) >= 0);
+
+      trace
+          .acc1(resHi.slice(0, 1 + i))
+          .acc2(resLo.slice(0, 1 + i))
+          .arg1Hi(arg1Hi)
+          .arg1Lo(arg1Lo)
+          .arg2Hi(arg2Hi)
+          .arg2Lo(arg2Lo)
+          .byte1(UnsignedByte.of(resHi.get(i)))
+          .byte2(UnsignedByte.of(resLo.get(i)))
+          .ct(Bytes.of(i))
+          .inst(Bytes.of(opCodeData.value()))
+          .overflow(overflowBit(i, overflowHi, overflowLo))
+          .resHi(resHi)
+          .resLo(resLo)
+          .stamp(Bytes.ofUnsignedLong(stamp))
+          .validateRow();
+    }
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hashCode(opCode, arg1, arg2);
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+    final AddOperation that = (AddOperation) o;
+    return java.util.Objects.equals(this.opCode, that.opCode)
+        && java.util.Objects.equals(this.rawArg1, that.rawArg1)
+        && java.util.Objects.equals(this.rawArg2, that.rawArg2);
+  }
+}

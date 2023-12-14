c29e5d34e1ef83b87557ea8c55707b38fa052be2
Tsvetan Dimitrov <tsvetan.dimitrov@consensys.net>
2023-12-14 14:18:53 +0200

    feat(mmio): add ExceptionalRamToStack3To2FullDispatcher, ExceptionalRamToStack3To2FullFastDispatcher, NaRamToStack2To1FullAndZeroDispatcher

---
+++ arithmetization/src/main/java/net/consensys/linea/zktracer/module/mmio/dispatchers/ExceptionalRamToStack3To2FullFastDispatcher.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright Consensys Software Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package net.consensys.linea.zktracer.module.mmio.dispatchers;
+
+import com.google.common.base.Preconditions;
+import net.consensys.linea.zktracer.module.mmio.MmioData;
+import net.consensys.linea.zktracer.module.mmu.MicroData;
+import net.consensys.linea.zktracer.runtime.callstack.CallStack;
+import net.consensys.linea.zktracer.types.UnsignedByte;
+
+public class ExceptionalRamToStack3To2FullFastDispatcher implements MmioDispatcher {
+  @Override
+  public MmioData dispatch(MicroData microData, CallStack callStack) {
+    MmioData mmioData = new MmioData();
+
+    mmioData.cnA(0);
+    mmioData.cnB(0);
+    mmioData.cnC(0);
+
+    mmioData.indexA(0);
+    mmioData.indexB(0);
+    mmioData.indexC(0);
+
+    Preconditions.checkState(
+        microData.isRootContext() && microData.isType5(),
+        "Should be: EXCEPTIONAL_RAM_TO_STACK_3_TO_2_FULL");
+
+    mmioData.valA(microData.valACache());
+    mmioData.valB(microData.valBCache());
+    mmioData.valC(microData.valCCache());
+
+    mmioData.valANew(UnsignedByte.EMPTY_BYTES16);
+    mmioData.valBNew(UnsignedByte.EMPTY_BYTES16);
+    mmioData.valCNew(UnsignedByte.EMPTY_BYTES16);
+
+    mmioData.valHi(mmioData.valA());
+    mmioData.valLo(mmioData.valB());
+
+    return mmioData;
+  }
+
+  @Override
+  public void update(MmioData mmioData, MicroData microData, int counter) {}
+}

35cb8ada635bf471e7c142b2e1f9dd7588153d16
Tsvetan Dimitrov <tsvetan.dimitrov@consensys.net>
2023-12-14 14:51:03 +0200

    feat(mmio): add FullStackToRamDispatcher, LsbFromStackToRamDispatcher, PushTwoStackToRamDispatcher

---
+++ arithmetization/src/main/java/net/consensys/linea/zktracer/module/mmio/dispatchers/FullStackToRamDispatcher.java
@@ -0,0 +1,78 @@
+/*
+ * Copyright Consensys Software Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package net.consensys.linea.zktracer.module.mmio.dispatchers;
+
+import net.consensys.linea.zktracer.module.mmio.MmioData;
+import net.consensys.linea.zktracer.module.mmu.MicroData;
+import net.consensys.linea.zktracer.runtime.callstack.CallStack;
+
+public class FullStackToRamDispatcher implements MmioDispatcher {
+  @Override
+  public MmioData dispatch(MicroData microData, CallStack callStack) {
+    MmioData mmioData = new MmioData();
+
+    int targetContext = microData.targetContext();
+    mmioData.cnA(targetContext);
+    mmioData.cnB(targetContext);
+    mmioData.cnC(targetContext);
+
+    int targetLimbOffset = microData.targetLimbOffset().toInt();
+    mmioData.indexA(targetLimbOffset);
+    mmioData.indexB(targetLimbOffset + 1);
+    mmioData.indexC(targetLimbOffset + 2);
+
+    mmioData.setVal(microData.eWordValue());
+
+    mmioData.valA(callStack.valueFromMemory(mmioData.cnA(), mmioData.indexA()));
+    mmioData.valB(callStack.valueFromMemory(mmioData.cnB(), mmioData.indexB()));
+    mmioData.valC(callStack.valueFromMemory(mmioData.cnC(), mmioData.indexC()));
+
+    int targetByteOffset = microData.targetByteOffset().toInteger();
+    for (int i = 0; i < 16; i++) {
+      if (i < targetByteOffset) {
+        mmioData.valANew()[i] = mmioData.valA()[i];
+        mmioData.valBNew()[i] = mmioData.valHi()[i + 16 - targetByteOffset];
+        mmioData.valCNew()[i] = mmioData.valLo()[i + 16 - targetByteOffset];
+      } else {
+        mmioData.valANew()[i] = mmioData.valHi()[i - targetByteOffset];
+        mmioData.valBNew()[i] = mmioData.valLo()[i - targetByteOffset];
+        mmioData.valCNew()[i] = mmioData.valC()[i];
+      }
+    }
+
+    mmioData.updateLimbsInMemory(callStack);
+
+    return mmioData;
+  }
+
+  @Override
+  public void update(MmioData mmioData, MicroData microData, int counter) {
+    // 2Full=>3
+    mmioData.twoFullToThree(
+        mmioData.byteA(counter),
+        mmioData.byteC(counter),
+        mmioData.byteHi(counter),
+        mmioData.byteLo(counter),
+        mmioData.acc1(),
+        mmioData.acc2(),
+        mmioData.acc3(),
+        mmioData.acc4(),
+        mmioData.acc5(),
+        mmioData.acc6(),
+        microData.targetByteOffset(),
+        counter);
+  }
+}
---
+++ arithmetization/src/main/java/net/consensys/linea/zktracer/module/mmio/dispatchers/LsbFromStackToRamDispatcher.java
@@ -0,0 +1,66 @@
+/*
+ * Copyright Consensys Software Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package net.consensys.linea.zktracer.module.mmio.dispatchers;
+
+import net.consensys.linea.zktracer.module.mmio.MmioData;
+import net.consensys.linea.zktracer.module.mmu.MicroData;
+import net.consensys.linea.zktracer.runtime.callstack.CallStack;
+import net.consensys.linea.zktracer.types.UnsignedByte;
+
+public class LsbFromStackToRamDispatcher implements MmioDispatcher {
+  @Override
+  public MmioData dispatch(MicroData microData, CallStack callStack) {
+    MmioData mmioData = new MmioData();
+
+    int targetContext = microData.targetContext();
+    mmioData.cnA(targetContext);
+    mmioData.cnB(0);
+    mmioData.cnC(0);
+
+    int targetLimbOffset = microData.targetLimbOffset().toInt();
+    mmioData.indexA(targetLimbOffset);
+    mmioData.indexB(0);
+    mmioData.indexC(0);
+
+    mmioData.valA(callStack.valueFromMemory(mmioData.cnA(), mmioData.indexA()));
+    mmioData.valB(UnsignedByte.EMPTY_BYTES16);
+    mmioData.valC(UnsignedByte.EMPTY_BYTES16);
+
+    mmioData.valBNew(UnsignedByte.EMPTY_BYTES16);
+    mmioData.valCNew(UnsignedByte.EMPTY_BYTES16);
+
+    mmioData.setVal(microData.eWordValue());
+
+    int targetByteOffset = microData.targetByteOffset().toInteger();
+    for (int i = 0; i < 16; i++) {
+      if (i == targetByteOffset) {
+        mmioData.valANew()[targetByteOffset] = mmioData.valLo()[15];
+      } else {
+        mmioData.valANew()[i] = mmioData.valA()[i];
+      }
+    }
+
+    mmioData.updateLimbsInMemory(callStack);
+
+    return mmioData;
+  }
+
+  @Override
+  public void update(MmioData mmioData, MicroData microData, int counter) {
+    mmioData.byteSwap(
+        mmioData.byteA(counter), mmioData.acc1(), microData.targetByteOffset(), counter);
+  }
+}

78376248cf48c9bd0b171374736754651bf38661
Florian Huc <florian.huc@consensys.net>
2023-12-19 12:25:45 +0100

    revert name change not to break downstream systems.

--- arithmetization/src/main/java/net/consensys/linea/zktracer/module/limits/precompiles/EcPairingNbEffectiveCall.java
+++ arithmetization/src/main/java/net/consensys/linea/zktracer/module/limits/precompiles/EcPairingNbEffectiveCall.java
@@ -42,7 +42,7 @@ public final class EcPairingNbEffectiveCall implements Module {
 
   @Override
   public String moduleKey() {
-    return "PRECOMPILE_ECPAIRING_EFFECTIVE_NB_CALL";
+    return "PRECOMPILE_ECPAIRING";
   }
 
   @Override

ce249e5566ebd00b303eb5117e2e759a80d97484
FlorianHuc <florian.huc@gmail.com>
2024-01-19 08:41:41 +0100

    fix stackedSet

--- arithmetization/src/test/java/net/consensys/linea/zktracer/container/stacked/list/StackedListTest.java
+++ arithmetization/src/test/java/net/consensys/linea/zktracer/container/stacked/list/StackedListTest.java
@@ -1,22 +1,4 @@
-/*
- * Copyright ConsenSys AG.
- *
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
- * the License. You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
- * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
- * specific language governing permissions and limitations under the License.
- *
- * SPDX-License-Identifier: Apache-2.0
- */
-
-package net.consensys.linea.zktracer.containers;
-
-import java.math.BigInteger;
-import java.util.List;
+package net.consensys.linea.zktracer.container.stacked.list;
 
 import com.google.common.collect.ImmutableList;
 import net.consensys.linea.zktracer.container.stacked.set.StackedSet;
@@ -26,7 +8,11 @@ import org.apache.tuweni.bytes.Bytes;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-public class StackedSetTests {
+import java.math.BigInteger;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StackedListTest {
 
   private final static AddOperation ONE_PLUS_ONE =
           new AddOperation(
@@ -41,31 +27,31 @@ public class StackedSetTests {
                   Bytes.wrap(BigInteger.TWO.toByteArray()));
   @Test
   public void push() {
-    StackedSet<AddOperation> chunks = new StackedSet<>();
+    StackedList<AddOperation> chunks = new StackedList<>();
     chunks.enter();
 
     chunks.add(ONE_PLUS_ONE);
     chunks.add(ONE_PLUS_ONE);
     chunks.add(ONE_PLUS_ONE);
-    Assertions.assertEquals(1, chunks.size());
+    Assertions.assertEquals(3, chunks.size());
     chunks.pop();
     Assertions.assertEquals(0, chunks.size());
   }
 
   @Test
   public void multiplePushPop() {
-    StackedSet<AddOperation> chunks = new StackedSet<>();
+    StackedList<AddOperation> chunks = new StackedList<>();
     chunks.enter();
     chunks.add(ONE_PLUS_ONE);
     chunks.add(ONE_PLUS_ONE);
-    Assertions.assertEquals(1, chunks.size());
+    Assertions.assertEquals(2, chunks.size());
 
     chunks.enter();
     chunks.add(ONE_PLUS_ONE);
-    Assertions.assertEquals(1, chunks.size());
+    Assertions.assertEquals(3, chunks.size());
     chunks.add(ONE_PLUS_TWO);
-    Assertions.assertEquals(2, chunks.size());
+    Assertions.assertEquals(4, chunks.size());
     chunks.pop();
-    Assertions.assertEquals(1, ImmutableList.copyOf(chunks.iterator()).size());
+    Assertions.assertEquals(2, ImmutableList.copyOf(chunks.iterator()).size());
   }
 }

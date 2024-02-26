5b191152251a085a7b634d662fbbf98fcc46902d
Franklin Delehelle <franklin.delehelle@odena.eu>
2024-02-26 10:51:02 +0100

    fix: make the build script portable, explicit dependency on Go && GCC

--- native/build.sh
+++ native/build.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/bin/sh
 
 # Initialize external vars - need this to get around unbound variable errors
 SKIP_GRADLE="$SKIP_GRADLE"
@@ -12,68 +12,31 @@ set -o errexit
 # Use the error status of the first failure, rather than that of the last item in a pipeline.
 set -o pipefail
 
-# Resolve the directory that contains this script. We have to jump through a few
-# hoops for this because the usual one-liners for this don't work if the script
-# is a symlink
-SOURCE="${BASH_SOURCE[0]}"
-while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
-  DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
-  SOURCE="$(readlink "$SOURCE")"
-  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
-done
-SCRIPTDIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
 
-# Determine core count for parallel make
-if [[ "$OSTYPE" == "linux-gnu" ]];  then
-  CORE_COUNT=$(nproc)
-  OSARCH=${OSTYPE%%[0-9.]*}-`arch`
+SCRIPTDIR=$( dirname -- "$( readlink -f -- "$0"; )"; )
+
+# delete old build dir, if exists
+rm -rf "$SCRIPTDIR/compress/build/native" || true
+mkdir -p "$SCRIPTDIR/compress/build/native"
+
+if [[ "$OSTYPE" == "msys" ]]; then
+  LIBRARY_EXTENSION=dll
+elif [[ "$OSTYPE" == "linux-gnu"* ]]; then
+  LIBRARY_EXTENSION=so
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+  LIBRARY_EXTENSION=dylib
+else
+  echo "*** Unknown OS: $OSTYPE"
+  exit 1
 fi
 
-if [[ "$OSTYPE" == "darwin"* ]];  then
-  CORE_COUNT=$(sysctl -n hw.ncpu)
-  if [[ "`machine`" == "arm"* ]]; then
-    arch_name="aarch64"
-    export CFLAGS="-arch arm64"
-  else
-    arch_name="x86-64"
-    export CFLAGS="-arch x86_64"
-  fi
-  OSARCH="darwin-$arch_name"
+
+if ! command -v go &> /dev/null; then
+    echo "*** The Go compiler could not be found"
+    exit 1
 fi
 
-# add to path cargo
-[ -f $HOME/.cargo/env ] && . $HOME/.cargo/env
-
-# add to path brew
-[ -f $HOME/.zprofile ] && . $HOME/.zprofile
-
-build_compress() {
-  cat <<EOF
-  ############################
-  ####### build compress #######
-  ############################
-EOF
-
-  cd "$SCRIPTDIR/compress/compress-jni"
-
-  # delete old build dir, if exists
-  rm -rf "$SCRIPTDIR/compress/build" || true
-  mkdir -p "$SCRIPTDIR/compress/build/lib"
-
-  if [[ "$OSTYPE" == "msys" ]]; then
-    	LIBRARY_EXTENSION=dll
-  elif [[ "$OSTYPE" == "linux-gnu"* ]]; then
-    LIBRARY_EXTENSION=so
-  elif [[ "$OSTYPE" == "darwin"* ]]; then
-    LIBRARY_EXTENSION=dylib
-  fi
-
-  go build -buildmode=c-shared -o libcompress_jni.$LIBRARY_EXTENSION compress-jni.go
-
-  mkdir -p "$SCRIPTDIR/compress/build/${OSARCH}/lib"
-  cp libcompress_jni.* "$SCRIPTDIR/compress/build/${OSARCH}/lib"
-}
-
-build_compress
-
-exit
+cd "$SCRIPTDIR/compress/compress-jni"
+echo "Building Go module libcompress_jni.$LIBRARY_EXTENSION for $OSTYPE"
+CGO_ENABLED=1 go build -buildmode=c-shared -o libcompress_jni.$LIBRARY_EXTENSION compress-jni.go
+mv libcompress_jni.* "$SCRIPTDIR/compress/build/native"

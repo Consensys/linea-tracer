e73530d5863447d511d235b5abb417a25a8e6230
Francois Bojarski <francois.bojarski@consensys.net>
2024-03-27 11:27:38 +0530

    test: add txInit and Create test

--- arithmetization/src/test/java/net/consensys/linea/zktracer/module/mmu/MemoryTests.java
+++ arithmetization/src/test/java/net/consensys/linea/zktracer/module/mmu/MemoryTests.java
@@ -15,15 +15,31 @@
 
 package net.consensys.linea.zktracer.module.mmu;
 
+import java.util.Random;
+
 import net.consensys.linea.zktracer.opcode.OpCode;
 import net.consensys.linea.zktracer.testing.BytecodeCompiler;
 import net.consensys.linea.zktracer.testing.BytecodeRunner;
 import net.consensys.linea.zktracer.testing.EvmExtension;
+import net.consensys.linea.zktracer.testing.ToyAccount;
+import net.consensys.linea.zktracer.testing.ToyExecutionEnvironment;
+import net.consensys.linea.zktracer.testing.ToyTransaction;
+import net.consensys.linea.zktracer.testing.ToyWorld;
+import org.apache.tuweni.bytes.Bytes;
+import org.hyperledger.besu.crypto.KeyPair;
+import org.hyperledger.besu.crypto.SECP256K1;
+import org.hyperledger.besu.datatypes.Address;
+import org.hyperledger.besu.datatypes.Hash;
+import org.hyperledger.besu.datatypes.TransactionType;
+import org.hyperledger.besu.datatypes.Wei;
+import org.hyperledger.besu.ethereum.core.Transaction;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 
 @ExtendWith(EvmExtension.class)
 class MemoryTests {
+  private final Random rnd = new Random(666);
+
   @Test
   void fastMstore() {
     BytecodeRunner.of(BytecodeCompiler.newProgram().push(25).push(32).op(OpCode.MSTORE).compile())
@@ -58,6 +74,123 @@ class MemoryTests {
   void nonAlignedMstore8() {
     BytecodeRunner.of(
             BytecodeCompiler.newProgram().push(66872).push(35).op(OpCode.MSTORE8).compile())
+        .run();
+  }
+
+  @Test
+  void extcodecopyThenCreate(){
+    ToyWorld.ToyWorldBuilder world = ToyWorld.builder();
+
+    // create called account
+    final ToyAccount calledAccount =
+      ToyAccount.builder()
+        .balance(Wei.of(rnd.nextInt(10000, 10000000)))
+        .nonce(rnd.nextInt(2, 500))
+        .address(Address.wrap(Bytes.random(20)))
+        .code(Bytes.random(1000))
+        .build();
+    world.account(calledAccount);
+
+    // create sender account
+    final KeyPair keyPair = new SECP256K1().generateKeyPair();
+    final Address senderAddress =
+      Address.extract(Hash.hash(keyPair.getPublicKey().getEncodedBytes()));
+    final ToyAccount senderAccount =
+      ToyAccount.builder()
+        .balance(Wei.of(rnd.nextLong(30000, 100000000)))
+        .nonce(rnd.nextInt(0, 500))
+        .address(senderAddress)
+        .build();
+    world.account(senderAccount);
+
+    final int lengthExtcodecopy = 133;
+    final int offsetExtcodecopy = 0;
+    final int destoffsetExtcodecopy = 16;
+
+    final int lengthCreate = 65;
+    final int offsetCreate = 3;
+    final int valueCreate = 0;
+
+    // create transaction
+    final Transaction tx =
+      ToyTransaction.builder()
+        .sender(senderAccount)
+        .keyPair(keyPair)
+        .transactionType(TransactionType.FRONTIER)
+        .gasLimit(rnd.nextLong(210000, 0xfffffffffffffL))
+        .value(Wei.of(rnd.nextInt(0, 1000)))
+        .payload( BytecodeCompiler.newProgram()
+          .push(lengthExtcodecopy)
+          .push(offsetExtcodecopy)
+          .push(destoffsetExtcodecopy)
+          .push(calledAccount.getAddress())
+          .op(OpCode.EXTCODECOPY)
+          .push(lengthCreate)
+          .push(offsetCreate)
+          .push(valueCreate)
+          .op(OpCode.CREATE)
+          .compile())
+        .build();
+
+    // build and run
+    ToyExecutionEnvironment.builder()
+      .toyWorld(world.build())
+      .transaction(tx)
+      .testValidator(x -> {})
+      .build()
+      .run();
+
+  }
+
+  @Test
+  void txInitialisationTriggersMmu() {
+    ToyWorld.ToyWorldBuilder world = ToyWorld.builder();
+
+    // create receiver account
+    final ToyAccount receiverAccount =
+        ToyAccount.builder()
+            .balance(Wei.of(rnd.nextInt(10000, 10000000)))
+            .nonce(rnd.nextInt(2, 500))
+            .address(Address.wrap(Bytes.random(20)))
+            .code(
+                BytecodeCompiler.newProgram()
+                    .push(32, 0xbeef)
+                    .push(32, 0xdead)
+                    .op(OpCode.ADD)
+                    .compile())
+            .build();
+    world.account(receiverAccount);
+
+    // create sender account
+    final KeyPair keyPair = new SECP256K1().generateKeyPair();
+    final Address senderAddress =
+        Address.extract(Hash.hash(keyPair.getPublicKey().getEncodedBytes()));
+    final ToyAccount senderAccount =
+        ToyAccount.builder()
+            .balance(Wei.of(rnd.nextLong(30000, 100000000)))
+            .nonce(rnd.nextInt(0, 500))
+            .address(senderAddress)
+            .build();
+    world.account(senderAccount);
+
+    // create transaction
+    final Transaction tx =
+        ToyTransaction.builder()
+            .sender(senderAccount)
+            .to(receiverAccount)
+            .keyPair(keyPair)
+            .transactionType(TransactionType.FRONTIER)
+            .gasLimit(rnd.nextLong(21000, 0xfffffffffffffL))
+            .value(Wei.of(rnd.nextInt(0, 1000)))
+            .payload(Bytes.random(rnd.nextInt(1, 1000)))
+            .build();
+
+    // build and run
+    ToyExecutionEnvironment.builder()
+        .toyWorld(world.build())
+        .transaction(tx)
+        .testValidator(x -> {})
+        .build()
         .run();
   }
 }

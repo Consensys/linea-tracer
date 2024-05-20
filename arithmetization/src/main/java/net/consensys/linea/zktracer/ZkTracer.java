551c2a38ee2f54075534d82f243c44518957cc4a
Ameziane H <ameziane.hamlat@consensys.net>
2024-05-20 16:41:52 +0200

    @ahamlat Improve ZkTracer initialization time by doing only once Opcodes and spillings loading from disk resources
    Signed-off-by: Ameziane H <ameziane.hamlat@consensys.net>

--- arithmetization/src/main/java/net/consensys/linea/zktracer/ZkTracer.java
+++ arithmetization/src/main/java/net/consensys/linea/zktracer/ZkTracer.java
@@ -38,10 +38,8 @@ import net.consensys.linea.config.LineaL1L2BridgeConfiguration;
 import net.consensys.linea.zktracer.module.Module;
 import net.consensys.linea.zktracer.module.Pin55;
 import net.consensys.linea.zktracer.module.hub.Hub;
-import net.consensys.linea.zktracer.opcode.OpCodes;
+import net.consensys.linea.zktracer.types.Utils;
 import org.apache.tuweni.bytes.Bytes;
-import org.apache.tuweni.toml.Toml;
-import org.apache.tuweni.toml.TomlTable;
 import org.hyperledger.besu.datatypes.Hash;
 import org.hyperledger.besu.datatypes.PendingTransaction;
 import org.hyperledger.besu.datatypes.Transaction;
@@ -62,10 +60,22 @@ public class ZkTracer implements ConflationAwareOperationTracer {
   public static final GasCalculator gasCalculator = new LondonGasCalculator();
 
   public static final FeeMarket feeMarket = FeeMarket.london(-1);
+  private static final Map<String, Integer> spillings;
+
+  static {
+    try {
+      // Load spillings configured in src/main/resources/spillings.toml.
+      spillings = Utils.computeSpillings();
+    } catch (final Exception e) {
+      final String errorMsg =
+          "A problem happened during spillings initialization, cause " + e.getCause();
+      log.error(errorMsg);
+      throw new RuntimeException(e);
+    }
+  }
 
   @Getter private final Hub hub;
   private final Optional<Pin55> pin55;
-  private final Map<String, Integer> spillings = new HashMap<>();
   private Hash hashOfLastTransactionTraced = Hash.EMPTY;
   /** Accumulate all the exceptions that happened at tracing time. */
   @Getter private final List<Exception> tracingExceptions = new FiniteList<>(50);
@@ -76,27 +86,12 @@ public class ZkTracer implements ConflationAwareOperationTracer {
 
   public ZkTracer(final LineaL1L2BridgeConfiguration bridgeConfiguration) {
     this.hub = new Hub(bridgeConfiguration.contract(), bridgeConfiguration.topic());
-
-    // Load opcodes configured in src/main/resources/opcodes.yml.
-    OpCodes.load();
-
-    // Load spillings configured in src/main/resources/spillings.toml.
-    try {
-      final TomlTable table =
-          Toml.parse(getClass().getClassLoader().getResourceAsStream("spillings.toml"))
-              .getTable("spillings");
-      table.toMap().keySet().forEach(k -> spillings.put(k, Math.toIntExact(table.getLong(k))));
-
-      for (Module m : this.hub.getModulesToCount()) {
-        if (!this.spillings.containsKey(m.moduleKey())) {
-          throw new IllegalStateException(
-              "Spilling for module " + m.moduleKey() + " not defined in spillings.toml");
-        }
+    for (Module m : this.hub.getModulesToCount()) {
+      if (!spillings.containsKey(m.moduleKey())) {
+        throw new IllegalStateException(
+            "Spilling for module " + m.moduleKey() + " not defined in spillings.toml");
       }
-    } catch (final Exception e) {
-      throw new RuntimeException(e);
     }
-
     // >>>> CHANGE ME >>>>
     // >>>> CHANGE ME >>>>
     // >>>> CHANGE ME >>>>
@@ -326,7 +321,7 @@ public class ZkTracer implements ConflationAwareOperationTracer {
                 modulesLineCount.put(
                     m.moduleKey(),
                     m.lineCount()
-                        + Optional.ofNullable(this.spillings.get(m.moduleKey()))
+                        + Optional.ofNullable(spillings.get(m.moduleKey()))
                             .orElseThrow(
                                 () ->
                                     new IllegalStateException(

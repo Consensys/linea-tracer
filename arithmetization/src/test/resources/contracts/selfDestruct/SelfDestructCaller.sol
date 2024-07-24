98ee0952c7d331c48050b163f67601a99113f82e
Lorenzo Gentile <lorenzogentile404@gmail.com>
2024-07-24 12:49:27 +0200

    feat(selfdestruct): fixed contracts for testing using 0.8.0+commit.c7dfd78e compiler version and VM London Environment

--- arithmetization/src/test/resources/contracts/selfDestruct/SelfDestructCaller.sol
+++ arithmetization/src/test/resources/contracts/selfDestruct/SelfDestructCaller.sol
@@ -2,31 +2,50 @@
 pragma solidity ^0.8.0;
 
 contract SelfDestructCaller {
-    
-    function staticCallToBWhenWarm(address bAddress) external {
-        // Get the balance of contract B to warm it
-        uint256 balance = address(bAddress).balance;
-        emit BalanceOfB(balance);
+    function callSelfDestructCalleeWhenWarm(address selfDestructCalleeAddress)
+        external
+    {
+        // Get the balance of SelfDestructCallee to warm it
+        emit Balance(address(selfDestructCalleeAddress).balance);
 
+        // Get the codesize of SelfDestructCallee
         uint256 size;
         assembly {
-            size := extcodesize(bAddress)
+            size := extcodesize(selfDestructCalleeAddress)
         }
         emit ExtCodeSize(size);
 
         // Perform the STATICCALL
-        (bool success, ) = bAddress.call(abi.encodeWithSignature("invokeSelfDestruct()"));
+        // (bool success, ) = selfDestructCalleeAddress.staticcall(abi.encodeWithSignature("invokeSelfDestruct()")); // fails
+        // emit CallSuccess(success);
+
+        // Perform the CALL
+        (bool success, ) = selfDestructCalleeAddress.call(
+            abi.encodeWithSignature("invokeSelfDestruct()")
+        ); // succeed
         emit CallSuccess(success);
 
+        // here codesize of SelfDestructCallee is not 0 yet
+    }
+
+    function verifyBalanceAndCodesizeOfSelfDestructCallee(
+        address selfDestructCalleeAddress
+    ) external {
+        // here codesize of SelfDestructCallee is 0 after invoking callSelfDestructCalleeWhenWarm
+        // Get the balance of SelfDestructCallee
+        emit Balance(address(selfDestructCalleeAddress).balance);
+
+        // Get the codesize of SelfDestructCallee
+        uint256 size;
         assembly {
-            size := extcodesize(bAddress)
+            size := extcodesize(selfDestructCalleeAddress)
         }
         emit ExtCodeSize(size);
     }
 
-    event BalanceOfB(uint256 balance);
-
-    event CallSuccess(bool success);
+    event Balance(uint256 balance);
 
     event ExtCodeSize(uint256 size);
+
+    event CallSuccess(bool success);
 }

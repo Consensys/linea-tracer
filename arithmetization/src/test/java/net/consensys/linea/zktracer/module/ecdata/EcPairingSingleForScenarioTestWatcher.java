3a3e48ddada010c686abc6fa78b22dd4461bcda0
Lorenzo Gentile <lorenzogentile404@gmail.com>
2024-07-28 19:12:56 +0200

    feat(ecpairing): create temp file in test watcher

--- arithmetization/src/test/java/net/consensys/linea/zktracer/module/ecdata/EcPairingSingleForScenarioTestWatcher.java
+++ arithmetization/src/test/java/net/consensys/linea/zktracer/module/ecdata/EcPairingSingleForScenarioTestWatcher.java
@@ -18,6 +18,8 @@ package net.consensys.linea.zktracer.module.ecdata;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.lang.reflect.Method;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Optional;
@@ -26,23 +28,23 @@ import org.junit.jupiter.api.extension.ExtensionContext;
 import org.junit.jupiter.api.extension.TestWatcher;
 
 public class EcPairingSingleForScenarioTestWatcher implements TestWatcher {
-  private static final String SUCCESSFUL_FILE_PATH;
-  private static final String FAILED_FILE_PATH;
+  private static final Path SUCCESSFUL_FILE_PATH;
+  private static final Path FAILED_FILE_PATH;
 
   static {
     String timestamp = new SimpleDateFormat("yyyyMMddHHmmss").format(new Date());
-    SUCCESSFUL_FILE_PATH =
-        String.format(
-            "../arithmetization/src/test/resources/ecpairing/test_ec_pairing_single_for_scenario_successful_%s.csv",
-            timestamp);
-    FAILED_FILE_PATH =
-        String.format(
-            "../arithmetization/src/test/resources/ecpairing/test_ec_pairing_single_for_scenario_failed_%s.csv",
-            timestamp);
-    try (FileWriter writerPassed = new FileWriter(SUCCESSFUL_FILE_PATH);
-        FileWriter writerFailed = new FileWriter(FAILED_FILE_PATH)) {
-      writerPassed.append("Ax,Ay,BxIm,BxRe,ByIm,ByRe\n");
-      writerFailed.append("Ax,Ay,BxIm,BxRe,ByIm,ByRe\n");
+    try {
+      SUCCESSFUL_FILE_PATH =
+          Files.createTempFile(
+              "test_ec_pairing_single_for_scenario_successful_" + timestamp, ".csv");
+      FAILED_FILE_PATH =
+          Files.createTempFile("test_ec_pairing_single_for_scenario_failed_" + timestamp, ".csv");
+
+      try (FileWriter writerPassed = new FileWriter(SUCCESSFUL_FILE_PATH.toFile());
+          FileWriter writerFailed = new FileWriter(FAILED_FILE_PATH.toFile())) {
+        writerPassed.append("Ax,Ay,BxIm,BxRe,ByIm,ByRe\n");
+        writerFailed.append("Ax,Ay,BxIm,BxRe,ByIm,ByRe\n");
+      }
     } catch (IOException e) {
       throw new RuntimeException(e);
     }
@@ -50,21 +52,25 @@ public class EcPairingSingleForScenarioTestWatcher implements TestWatcher {
 
   @Override
   public void testSuccessful(ExtensionContext context) {
+    System.out.println(
+        "EcPairingSingleForScenarioTestWatcher\nSUCCESSFUL_FILE_PATH = " + SUCCESSFUL_FILE_PATH);
     logResult(context, SUCCESSFUL_FILE_PATH);
   }
 
   @Override
   public void testFailed(ExtensionContext context, Throwable cause) {
+    System.out.println(
+        "EcPairingSingleForScenarioTestWatcher\nFAILED_FILE_PATH = " + FAILED_FILE_PATH);
     logResult(context, FAILED_FILE_PATH);
   }
 
-  private void logResult(ExtensionContext context, String filePath) {
+  private void logResult(ExtensionContext context, Path filePath) {
     Optional<Method> testMethod = context.getTestMethod();
     if (testMethod.isPresent()
         && (testMethod.get().getName().equals("testEcPairingSingleForScenarioUsingMethodSource")
             || testMethod.get().getName().equals("testEcPairingSingleForScenarioUsingCsv"))) {
       String params = context.getDisplayName().replaceAll("\\[\\d+] ", "");
-      try (FileWriter writer = new FileWriter(filePath, true)) {
+      try (FileWriter writer = new FileWriter(filePath.toFile(), true)) {
         writer.append(params).append("\n");
       } catch (IOException e) {
         throw new RuntimeException(e);

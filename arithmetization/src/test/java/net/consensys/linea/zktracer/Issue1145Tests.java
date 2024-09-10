fcb7e342006034e1dde408e04416dd63743f8e7b
Olivier Bégassat <olivier.begassat.cours@gmail.com>
2024-09-10 14:09:23 +0200

    feat(tests): added new replay file (which, again, is unexceptional)

---
+++ arithmetization/src/test/java/net/consensys/linea/zktracer/Issue1145Tests.java
@@ -0,0 +1,27 @@
+/*
+ * Copyright Consensys Software Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+package net.consensys.linea.zktracer;
+
+import org.junit.jupiter.api.Test;
+
+import static net.consensys.linea.zktracer.ReplayTests.replay;
+
+public class Issue1145Tests {
+
+    @Test
+    public void issue_1145_block_3318494_ReturnPrecondition() {
+        replay("3318494-3318494.json.gz", false);
+    }
+}

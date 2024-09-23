5d1d8ffe426044e5d9272555e69613dd292a476e
Gaurav Ahuja <gauravahuja9@gmail.com>
2024-09-23 13:23:53 +0530

    Fix multi block tests

--- testing/src/main/java/net/consensys/linea/testing/MultiBlockExecutionEnvironment.java
+++ testing/src/main/java/net/consensys/linea/testing/MultiBlockExecutionEnvironment.java
@@ -15,8 +15,10 @@
 
 package net.consensys.linea.testing;
 
+import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Optional;
 
@@ -24,15 +26,35 @@ import lombok.Builder;
 import lombok.Singular;
 import lombok.extern.slf4j.Slf4j;
 import net.consensys.linea.blockcapture.snapshots.*;
+import net.consensys.linea.corset.CorsetValidator;
+import net.consensys.linea.zktracer.ConflationAwareOperationTracer;
+import net.consensys.linea.zktracer.ZkTracer;
+import org.apache.tuweni.bytes.Bytes;
+import org.apache.tuweni.units.bigints.UInt256;
+import org.hyperledger.besu.datatypes.Address;
+import org.hyperledger.besu.datatypes.Wei;
 import org.hyperledger.besu.ethereum.core.*;
+import org.hyperledger.besu.ethereum.mainnet.MainnetTransactionProcessor;
+import org.hyperledger.besu.ethereum.processing.TransactionProcessingResult;
+import org.hyperledger.besu.ethereum.referencetests.ReferenceTestWorldState;
+import org.hyperledger.besu.evm.account.MutableAccount;
+import org.hyperledger.besu.evm.internal.EvmConfiguration;
+import org.hyperledger.besu.evm.internal.Words;
+import org.hyperledger.besu.evm.operation.BlockHashOperation;
+import org.hyperledger.besu.evm.tracing.OperationTracer;
+import org.hyperledger.besu.evm.worldstate.WorldUpdater;
 
 @Builder
 @Slf4j
 public class MultiBlockExecutionEnvironment {
+  private static final CorsetValidator CORSET_VALIDATOR = new CorsetValidator();
+
   @Singular("addAccount")
   private final List<ToyAccount> accounts;
 
   private final List<BlockSnapshot> blocks;
+
+  private final ZkTracer tracer = new ZkTracer();
 
   public static class MultiBlockExecutionEnvironmentBuilder {
 
@@ -53,9 +75,8 @@ public class MultiBlockExecutionEnvironment {
   }
 
   public void run() {
-    ReplayExecutionEnvironment.builder()
-        .build()
-        .replay(ToyExecutionEnvironmentV2.CHAIN_ID, this.buildConflationSnapshot());
+    this.executeFrom(ToyExecutionEnvironmentV2.CHAIN_ID, this.buildConflationSnapshot());
+    ExecutionEnvironment.checkTracer(tracer, CORSET_VALIDATOR, Optional.of(log));
   }
 
   private ConflationSnapshot buildConflationSnapshot() {
@@ -94,5 +115,81 @@ public class MultiBlockExecutionEnvironment {
 
     return new ConflationSnapshot(
         this.blocks, accountSnapshots, storageSnapshots, blockHashSnapshots);
+  }
+
+  private void executeFrom(final BigInteger chainId, final ConflationSnapshot conflation) {
+    BlockHashOperation.BlockHashLookup blockHashLookup = conflation.toBlockHashLookup();
+    ReferenceTestWorldState world =
+        ReferenceTestWorldState.create(new HashMap<>(), EvmConfiguration.DEFAULT);
+    // Initialise world state from conflation
+    initWorld(world.updater(), conflation);
+    // Construct the transaction processor
+    final MainnetTransactionProcessor transactionProcessor =
+        ExecutionEnvironment.getProtocolSpec(chainId).getTransactionProcessor();
+    // Begin
+    tracer.traceStartConflation(conflation.blocks().size());
+    //
+    for (BlockSnapshot blockSnapshot : conflation.blocks()) {
+      final BlockHeader header = blockSnapshot.header().toBlockHeader();
+
+      final BlockBody body =
+          new BlockBody(
+              blockSnapshot.txs().stream().map(TransactionSnapshot::toTransaction).toList(),
+              new ArrayList<>());
+      tracer.traceStartBlock(header, body);
+
+      for (TransactionSnapshot txs : blockSnapshot.txs()) {
+        final Transaction tx = txs.toTransaction();
+        final WorldUpdater updater = world.updater();
+        // Process transaction leading to expected outcome
+        final TransactionProcessingResult outcome =
+            transactionProcessor.processTransaction(
+                updater,
+                header,
+                tx,
+                header.getCoinbase(),
+                buildOperationTracer(tx, txs.getOutcome()),
+                blockHashLookup,
+                false,
+                Wei.ZERO);
+        // Commit transaction
+        updater.commit();
+      }
+      tracer.traceEndBlock(header, body);
+    }
+    tracer.traceEndConflation(world.updater());
+  }
+
+  private OperationTracer buildOperationTracer(Transaction tx, TransactionResultSnapshot txs) {
+    if (txs == null) {
+      String hash = tx.getHash().toHexString();
+      log.info("tx `{}` outcome not checked (missing)", hash);
+      return tracer;
+    } else {
+      return ConflationAwareOperationTracer.sequence(txs.check(), tracer);
+    }
+  }
+
+  private static void initWorld(WorldUpdater world, final ConflationSnapshot conflation) {
+    WorldUpdater updater = world.updater();
+
+    for (AccountSnapshot account : conflation.accounts()) {
+      // Construct contract address
+      Address addr = Address.fromHexString(account.address());
+      // Create account
+      MutableAccount acc =
+          world.createAccount(
+              Words.toAddress(addr), account.nonce(), Wei.fromHexString(account.balance()));
+      // Update code
+      acc.setCode(Bytes.fromHexString(account.code()));
+    }
+    // Initialise storage
+    for (StorageSnapshot s : conflation.storage()) {
+      world
+          .getAccount(Words.toAddress(Bytes.fromHexString(s.address())))
+          .setStorageValue(UInt256.fromHexString(s.key()), UInt256.fromHexString(s.value()));
+    }
+    //
+    world.commit();
   }
 }

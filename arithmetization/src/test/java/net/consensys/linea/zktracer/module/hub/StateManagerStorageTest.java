bf596c05bdf95ada070199a1d6189745aeadbcbe
arijitdutta67 <arijit.dutta67@gmail.com>
2024-10-01 13:17:27 +0530

    Adding storage test with warming of accounts

--- arithmetization/src/test/java/net/consensys/linea/zktracer/module/hub/StateManagerStorageTest.java
+++ arithmetization/src/test/java/net/consensys/linea/zktracer/module/hub/StateManagerStorageTest.java
@@ -24,18 +24,59 @@ import net.consensys.linea.zktracer.opcode.OpCode;
 import org.apache.tuweni.bytes.Bytes32;
 import org.hyperledger.besu.crypto.KeyPair;
 import org.hyperledger.besu.crypto.SECP256K1;
-import org.hyperledger.besu.datatypes.Address;
-import org.hyperledger.besu.datatypes.Hash;
-import org.hyperledger.besu.datatypes.Wei;
+import org.hyperledger.besu.datatypes.*;
 import org.hyperledger.besu.ethereum.core.Transaction;
 import org.junit.jupiter.api.Test;
 
 public class StateManagerStorageTest {
+
+  final KeyPair senderKeyPair1 = new SECP256K1().generateKeyPair();
+  final Address senderAddress1 =
+      Address.extract(Hash.hash(senderKeyPair1.getPublicKey().getEncodedBytes()));
+  final ToyAccount senderAccount1 =
+      ToyAccount.builder().balance(Wei.fromEth(123)).nonce(5).address(senderAddress1).build();
+
+  final KeyPair senderKeyPair2 = new SECP256K1().generateKeyPair();
+  final Address senderAddress2 =
+      Address.extract(Hash.hash(senderKeyPair2.getPublicKey().getEncodedBytes()));
+  final ToyAccount senderAccount2 =
+      ToyAccount.builder().balance(Wei.fromEth(1231)).nonce(15).address(senderAddress2).build();
+
+  final KeyPair senderKeyPair3 = new SECP256K1().generateKeyPair();
+  final Address senderAddress3 =
+      Address.extract(Hash.hash(senderKeyPair3.getPublicKey().getEncodedBytes()));
+  final ToyAccount senderAccount3 =
+      ToyAccount.builder().balance(Wei.fromEth(1231)).nonce(15).address(senderAddress3).build();
+
+  final KeyPair senderKeyPair4 = new SECP256K1().generateKeyPair();
+  final Address senderAddress4 =
+      Address.extract(Hash.hash(senderKeyPair4.getPublicKey().getEncodedBytes()));
+  final ToyAccount senderAccount4 =
+      ToyAccount.builder().balance(Wei.fromEth(11)).nonce(115).address(senderAddress4).build();
+
+  final KeyPair senderKeyPair5 = new SECP256K1().generateKeyPair();
+  final Address senderAddress5 =
+      Address.extract(Hash.hash(senderKeyPair5.getPublicKey().getEncodedBytes()));
+  final ToyAccount senderAccount5 =
+      ToyAccount.builder().balance(Wei.fromEth(12)).nonce(0).address(senderAddress5).build();
+
+  final KeyPair senderKeyPair6 = new SECP256K1().generateKeyPair();
+  final Address senderAddress6 =
+      Address.extract(Hash.hash(senderKeyPair6.getPublicKey().getEncodedBytes()));
+  final ToyAccount senderAccount6 =
+      ToyAccount.builder().balance(Wei.fromEth(12)).nonce(6).address(senderAddress6).build();
+
+  final KeyPair senderKeyPair7 = new SECP256K1().generateKeyPair();
+  final Address senderAddress7 =
+      Address.extract(Hash.hash(senderKeyPair7.getPublicKey().getEncodedBytes()));
+  final ToyAccount senderAccount7 =
+      ToyAccount.builder().balance(Wei.fromEth(231)).nonce(21).address(senderAddress7).build();
   private final String keyString =
       "0x00010203040060708090A0B0C0DE0F10101112131415161718191A1B1C1D1E1F";
   private final Bytes32 key = Bytes32.fromHexString(keyString);
   private final Bytes32 value1 = Bytes32.repeat((byte) 1);
   private final Bytes32 value2 = Bytes32.repeat((byte) 2);
+
   private final Address receiverAddress =
       Address.fromHexString("0x00000bad0000000000000000000000000000b077");
   final ToyAccount receiverAccount =
@@ -59,59 +100,32 @@ public class StateManagerStorageTest {
                   .compile())
           .nonce(116)
           .build();
+  final List<String> simpleKey = List.of(keyString);
+  final List<String> duplicateKey = List.of(keyString, keyString);
+  final List<String> simpleKeyAndTrash =
+      List.of(keyString, "0xdeadbeefdeadbeefdeadbeefdeadbeefdeadbeefdeadbeefdeadbeefdeadbeef");
+
+  final List<AccessListEntry> warmOnlyReceiver =
+      List.of(AccessListEntry.createAccessListEntry(receiverAddress, simpleKey));
+
+  final List<AccessListEntry> stupidWarmer =
+      List.of(
+          AccessListEntry.createAccessListEntry(receiverAddress, duplicateKey),
+          AccessListEntry.createAccessListEntry(senderAddress4, simpleKeyAndTrash),
+          AccessListEntry.createAccessListEntry(senderAddress1, simpleKeyAndTrash),
+          AccessListEntry.createAccessListEntry(senderAddress3, simpleKey),
+          AccessListEntry.createAccessListEntry(senderAddress7, duplicateKey));
 
   @Test
-  void test() {
+  void test_simple() {
 
-    final KeyPair senderKeyPair1 = new SECP256K1().generateKeyPair();
-    final Address senderAddress1 =
-        Address.extract(Hash.hash(senderKeyPair1.getPublicKey().getEncodedBytes()));
     System.out.println("Sender address1: " + senderAddress1);
-    final ToyAccount senderAccount1 =
-        ToyAccount.builder().balance(Wei.fromEth(123)).nonce(5).address(senderAddress1).build();
-
-    final KeyPair senderKeyPair2 = new SECP256K1().generateKeyPair();
-    final Address senderAddress2 =
-        Address.extract(Hash.hash(senderKeyPair2.getPublicKey().getEncodedBytes()));
     System.out.println("Sender address2: " + senderAddress2);
-    final ToyAccount senderAccount2 =
-        ToyAccount.builder().balance(Wei.fromEth(1231)).nonce(15).address(senderAddress2).build();
-
-    final KeyPair senderKeyPair3 = new SECP256K1().generateKeyPair();
-    final Address senderAddress3 =
-        Address.extract(Hash.hash(senderKeyPair3.getPublicKey().getEncodedBytes()));
     System.out.println("Sender address3: " + senderAddress3);
-    final ToyAccount senderAccount3 =
-        ToyAccount.builder().balance(Wei.fromEth(1231)).nonce(15).address(senderAddress3).build();
-
-    final KeyPair senderKeyPair4 = new SECP256K1().generateKeyPair();
-    final Address senderAddress4 =
-        Address.extract(Hash.hash(senderKeyPair4.getPublicKey().getEncodedBytes()));
     System.out.println("Sender address4: " + senderAddress4);
-    final ToyAccount senderAccount4 =
-        ToyAccount.builder().balance(Wei.fromEth(11)).nonce(115).address(senderAddress4).build();
-
-    final KeyPair senderKeyPair5 = new SECP256K1().generateKeyPair();
-    final Address senderAddress5 =
-        Address.extract(Hash.hash(senderKeyPair5.getPublicKey().getEncodedBytes()));
     System.out.println("Sender address5: " + senderAddress5);
-    final ToyAccount senderAccount5 =
-        ToyAccount.builder().balance(Wei.fromEth(12)).nonce(0).address(senderAddress5).build();
-
-    final KeyPair senderKeyPair6 = new SECP256K1().generateKeyPair();
-    final Address senderAddress6 =
-        Address.extract(Hash.hash(senderKeyPair6.getPublicKey().getEncodedBytes()));
     System.out.println("Sender address6: " + senderAddress6);
-    final ToyAccount senderAccount6 =
-        ToyAccount.builder().balance(Wei.fromEth(12)).nonce(6).address(senderAddress6).build();
-
-    final KeyPair senderKeyPair7 = new SECP256K1().generateKeyPair();
-    final Address senderAddress7 =
-        Address.extract(Hash.hash(senderKeyPair7.getPublicKey().getEncodedBytes()));
     System.out.println("Sender address7: " + senderAddress7);
-    final ToyAccount senderAccount7 =
-        ToyAccount.builder().balance(Wei.fromEth(231)).nonce(21).address(senderAddress7).build();
-
     final Transaction pureTransferSender1 =
         ToyTransaction.builder()
             .sender(senderAccount1)
@@ -156,6 +170,66 @@ public class StateManagerStorageTest {
                 receiverAccount))
         .addBlock(List.of(pureTransferSender1, pureTransferSender2))
         .addBlock(List.of(pureTransferSender3, pureTransferSender4))
+        .build()
+        .run();
+  }
+
+  @Test
+  void test_warming() {
+    System.out.println("Sender address1: " + senderAddress1);
+    System.out.println("Sender address2: " + senderAddress2);
+    System.out.println("Sender address3: " + senderAddress3);
+    System.out.println("Sender address4: " + senderAddress4);
+    System.out.println("Sender address5: " + senderAddress5);
+    System.out.println("Sender address6: " + senderAddress6);
+    System.out.println("Sender address7: " + senderAddress7);
+    final Transaction simpleWarm =
+        ToyTransaction.builder()
+            .sender(senderAccount1)
+            .to(receiverAccount)
+            .keyPair(senderKeyPair1)
+            .gasLimit(1000000L)
+            .transactionType(TransactionType.ACCESS_LIST)
+            .accessList(warmOnlyReceiver)
+            .value(Wei.of(50000))
+            .build();
+
+    final Transaction stupidWarm =
+        ToyTransaction.builder()
+            .sender(senderAccount2)
+            .to(receiverAccount)
+            .keyPair(senderKeyPair2)
+            .gasLimit(1000000L)
+            .transactionType(TransactionType.ACCESS_LIST)
+            .accessList(stupidWarmer)
+            .value(Wei.of(50000))
+            .build();
+
+    final Transaction noWarm =
+        ToyTransaction.builder()
+            .sender(senderAccount3)
+            .to(receiverAccount)
+            .keyPair(senderKeyPair3)
+            .gasLimit(1000000L)
+            .transactionType(TransactionType.ACCESS_LIST)
+            .accessList(List.of())
+            .value(Wei.of(50000))
+            .build();
+    MultiBlockExecutionEnvironment.MultiBlockExecutionEnvironmentBuilder builder =
+        MultiBlockExecutionEnvironment.builder();
+    builder
+        .accounts(
+            List.of(
+                senderAccount1,
+                senderAccount2,
+                senderAccount3,
+                senderAccount4,
+                senderAccount5,
+                senderAccount6,
+                senderAccount7,
+                receiverAccount))
+        .addBlock(List.of(simpleWarm, noWarm))
+        .addBlock(List.of(stupidWarm))
         .build()
         .run();
   }

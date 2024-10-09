0963a7929ba6b888dfb2be6571d135d02d12da9e
Francois Bojarski <francois.bojarski@consensys.net>
2024-10-09 13:50:23 +0530

    spotless
    Signed-off-by: Francois Bojarski <francois.bojarski@consensys.net>

--- arithmetization/src/main/java/net/consensys/linea/zktracer/container/stacked/ModuleOperationStackedSet.java
+++ arithmetization/src/main/java/net/consensys/linea/zktracer/container/stacked/ModuleOperationStackedSet.java
@@ -20,7 +20,6 @@ import static com.google.common.base.Preconditions.checkState;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Comparator;
-import java.util.Iterator;
 import java.util.List;
 
 import lombok.Getter;

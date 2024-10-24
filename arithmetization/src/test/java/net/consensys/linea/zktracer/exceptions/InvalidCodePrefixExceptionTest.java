00d2c067930150a9406bebea4c91f6a1b9394f9d
Lorenzo Gentile <lorenzogentile404@gmail.com>
2024-10-24 20:34:51 +0200

    text(invalidCodePrefix)

--- arithmetization/src/test/java/net/consensys/linea/zktracer/exceptions/InvalidCodePrefixExceptionTest.java
+++ arithmetization/src/test/java/net/consensys/linea/zktracer/exceptions/InvalidCodePrefixExceptionTest.java
@@ -14,10 +14,54 @@
  */
 package net.consensys.linea.zktracer.exceptions;
 
+import static net.consensys.linea.zktracer.module.hub.signals.TracedException.INVALID_CODE_PREFIX;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import net.consensys.linea.testing.BytecodeCompiler;
+import net.consensys.linea.testing.BytecodeRunner;
+import net.consensys.linea.zktracer.opcode.OpCode;
 import org.junit.jupiter.api.Test;
 
 public class InvalidCodePrefixExceptionTest {
 
   @Test
-  void invalidCodePrefixExceptionTest() {}
+  void invalidCodePrefixExceptionViaContractCreationTest() {
+    BytecodeCompiler program = BytecodeCompiler.newProgram();
+
+    program
+      .push("ef" + "00".repeat(31))
+      .push(0)
+      .op(OpCode.MSTORE)
+      .push(32)
+      .push(0)
+      .push(0)
+      .op(OpCode.CREATE);
+
+    BytecodeRunner bytecodeRunner = BytecodeRunner.of(program.compile());
+    bytecodeRunner.run();
+
+    assertEquals(
+      INVALID_CODE_PREFIX,
+      bytecodeRunner.getHub().previousTraceSection(2).commonValues.tracedException());
+  }
+
+  @Test
+  void invalidCodePrefixExceptionViaReturnTest() {
+    BytecodeCompiler program = BytecodeCompiler.newProgram();
+
+    program
+        .push("ef" + "00".repeat(31))
+        .push(0)
+        .op(OpCode.MSTORE)
+        .push(1)
+        .push(0)
+        .op(OpCode.RETURN);
+
+    BytecodeRunner bytecodeRunner = BytecodeRunner.of(program.compile());
+    bytecodeRunner.run();
+
+    assertEquals(
+        INVALID_CODE_PREFIX,
+        bytecodeRunner.getHub().previousTraceSection(2).commonValues.tracedException());
+  }
 }

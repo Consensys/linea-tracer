60d77e06d9d838387f35d3817040e7cce51a1126
Lorenzo Gentile <lorenzogentile404@gmail.com>
2024-10-28 17:02:48 +0100

    feat(mxp): add size1NonzeroNoMxpx and size2NonzeroNoMxpx

--- arithmetization/src/main/java/net/consensys/linea/zktracer/module/mxp/MxpOperation.java
+++ arithmetization/src/main/java/net/consensys/linea/zktracer/module/mxp/MxpOperation.java
@@ -609,6 +609,8 @@ public class MxpOperation extends ModuleOperation {
           .gasMxp(Bytes.ofUnsignedLong(this.mxpCall.getGasMxp()))
           .expands(this.isExpands())
           .mtntop(this.mxpCall.mayTriggerNontrivialMmuOperation)
+          .size1NonzeroNoMxpx(!this.mxpCall.mxpx && !this.mxpCall.getSize1().isZero())
+          .size2NonzeroNoMxpx(!this.mxpCall.mxpx && !this.mxpCall.getSize2().isZero())
           .validateRow();
     }
   }

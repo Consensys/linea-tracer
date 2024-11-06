34be96584d040a56b0aaeeed5aa001b5b4fd53b0
Olivier Bégassat <olivier.begassat.cours@gmail.com>
2024-11-06 15:25:10 +0100

    ras: documentation for EmptyDeploymentsInTheRootTest

--- arithmetization/src/test/java/net/consensys/linea/zktracer/instructionprocessing/EmptyDeploymentsInTheRootTest.java
+++ arithmetization/src/test/java/net/consensys/linea/zktracer/instructionprocessing/EmptyDeploymentsInTheRootTest.java
@@ -32,6 +32,13 @@ import org.hyperledger.besu.datatypes.Wei;
 import org.hyperledger.besu.ethereum.core.Transaction;
 import org.junit.jupiter.api.Test;
 
+/**
+ * The purpose of {@link EmptyDeploymentsInTheRootTest} is to make sure that deployment
+ * transactions work and lead to the actual deployment of bytecode. In particular the final
+ * RETURN instruction (if present) should be treated properly and the update to the deployment
+ * account is accounted for in the relevant row.
+ *
+ */
 public class EmptyDeploymentsInTheRootTest {
 
   final Bytes initCodeEmptyDeployment =

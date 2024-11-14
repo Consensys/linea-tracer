290f05a42c5f82adcc7ea2eee83c93b25d682ba7
Olivier Bégassat <olivier.begassat.cours@gmail.com>
2024-11-14 16:18:15 +0700

    feat: straightforward CALL tests

--- arithmetization/src/test/java/net/consensys/linea/zktracer/instructionprocessing/KeccakTests.java
+++ arithmetization/src/test/java/net/consensys/linea/zktracer/instructionprocessing/KeccakTests.java
@@ -20,93 +20,87 @@ import net.consensys.linea.zktracer.opcode.OpCode;
 import org.junit.jupiter.api.Test;
 
 public class KeccakTests {
-    @Test
-    void singleEmptyKeccak() {
-        BytecodeRunner.of(
-                        BytecodeCompiler.newProgram()
-                                .push(0) // size
-                                .push(0) // offset
-                                .op(OpCode.SHA3)
-                                .op(OpCode.POP)
-                                .compile())
-                .run();
-    }
+  @Test
+  void singleEmptyKeccak() {
+    BytecodeRunner.of(
+            BytecodeCompiler.newProgram()
+                .push(0) // size
+                .push(0) // offset
+                .op(OpCode.SHA3)
+                .op(OpCode.POP)
+                .compile())
+        .run();
+  }
 
-    /**
-     * computing KEC("ee ee ... ee"), aligned on 1st byte
-     */
-    @Test
-    void singleWordKeccakNonAligned() {
-        BytecodeRunner.of(
-                        BytecodeCompiler.newProgram()
-                                .push("eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee")
-                                .push(1)
-                                .op(OpCode.MSTORE)
-                                .push(32) // size
-                                .push(1) // offset
-                                .op(OpCode.SHA3)
-                                .op(OpCode.POP)
-                                .compile())
-                .run();
-    }
+  /** computing KEC("ee ee ... ee"), aligned on 1st byte */
+  @Test
+  void singleWordKeccakNonAligned() {
+    BytecodeRunner.of(
+            BytecodeCompiler.newProgram()
+                .push("eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee")
+                .push(1)
+                .op(OpCode.MSTORE)
+                .push(32) // size
+                .push(1) // offset
+                .op(OpCode.SHA3)
+                .op(OpCode.POP)
+                .compile())
+        .run();
+  }
 
-    /**
-     * computing KEC("ee ee ... ee"), aligned on 0th byte
-     */
-    @Test
-    void singleWordKeccakAligned() {
-        BytecodeRunner.of(
-                        BytecodeCompiler.newProgram()
-                                .push("eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee")
-                                .push(0)
-                                .op(OpCode.MSTORE)
-                                .push(32) // size
-                                .push(0) // offset
-                                .op(OpCode.SHA3)
-                                .op(OpCode.POP)
-                                .compile())
-                .run();
-    }
+  /** computing KEC("ee ee ... ee"), aligned on 0th byte */
+  @Test
+  void singleWordKeccakAligned() {
+    BytecodeRunner.of(
+            BytecodeCompiler.newProgram()
+                .push("eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee")
+                .push(0)
+                .op(OpCode.MSTORE)
+                .push(32) // size
+                .push(0) // offset
+                .op(OpCode.SHA3)
+                .op(OpCode.POP)
+                .compile())
+        .run();
+  }
 
-    /**
-     * computing KEC("ee")
-     */
-    @Test
-    void singleByteKeccak() {
-        BytecodeRunner.of(
-                        BytecodeCompiler.newProgram()
-                                .push("ee")
-                                .push(1)
-                                .op(OpCode.MSTORE8)
-                                .push(1) // size
-                                .push(1) // offset
-                                .op(OpCode.SHA3)
-                                .op(OpCode.POP)
-                                .compile())
-                .run();
-    }
+  /** computing KEC("ee") */
+  @Test
+  void singleByteKeccak() {
+    BytecodeRunner.of(
+            BytecodeCompiler.newProgram()
+                .push("ee")
+                .push(1)
+                .op(OpCode.MSTORE8)
+                .push(1) // size
+                .push(1) // offset
+                .op(OpCode.SHA3)
+                .op(OpCode.POP)
+                .compile())
+        .run();
+  }
 
-    @Test
-    void testSeveralKeccaks() {
-        BytecodeRunner.of(
-                        BytecodeCompiler.newProgram()
-                                .push(0)
-                                .push(0)
-                                .op(OpCode.SHA3)
-                                .op(OpCode.POP)
-                                .push(64)
-                                .push(13)
-                                .op(OpCode.SHA3)
-                                .op(OpCode.POP)
-                                .push(11)
-                                .push(75)
-                                .op(OpCode.SHA3)
-                                .op(OpCode.POP)
-                                .push(32)
-                                .push(32)
-                                .op(OpCode.SHA3)
-                                .op(OpCode.POP)
-                                .compile())
-                .run();
-    }
+  @Test
+  void testSeveralKeccaks() {
+    BytecodeRunner.of(
+            BytecodeCompiler.newProgram()
+                .push(0)
+                .push(0)
+                .op(OpCode.SHA3)
+                .op(OpCode.POP)
+                .push(64)
+                .push(13)
+                .op(OpCode.SHA3)
+                .op(OpCode.POP)
+                .push(11)
+                .push(75)
+                .op(OpCode.SHA3)
+                .op(OpCode.POP)
+                .push(32)
+                .push(32)
+                .op(OpCode.SHA3)
+                .op(OpCode.POP)
+                .compile())
+        .run();
+  }
 }
---
+++ arithmetization/src/test/java/net/consensys/linea/zktracer/instructionprocessing/callTests/trimmingTests.java
@@ -0,0 +1,66 @@
+/*
+ * Copyright Consensys Software Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+package net.consensys.linea.zktracer.instructionprocessing.callTests;
+
+import static net.consensys.linea.zktracer.opcode.OpCode.*;
+
+import net.consensys.linea.testing.BytecodeCompiler;
+import net.consensys.linea.testing.BytecodeRunner;
+import org.apache.tuweni.bytes.Bytes;
+import org.junit.jupiter.api.Test;
+
+public class trimmingTests {
+
+  final String fullEoaAddress = "000000000000000000000000abcdef0123456789";
+  final String toTrim12 = "aaaaaaaaaaaaaaaaaaaaaaaa";
+  final String untrimmedEoaAddress = toTrim12 + fullEoaAddress;
+  final String eoaAddress = "abcdef0123456789";
+
+  @Test
+  void targetTrimming() {
+
+    Bytes bytecode =
+        BytecodeCompiler.newProgram()
+            .push(1)
+            .push(2)
+            .push(3)
+            .push(4)
+            .push(1) // value
+            .push(untrimmedEoaAddress) // address
+            .push(0) // gas
+            .op(CALL)
+            .op(POP)
+            .push(untrimmedEoaAddress) // address
+            .op(BALANCE)
+            .push(eoaAddress) // address
+            .op(BALANCE)
+            .push(1)
+            .push(2)
+            .push(3)
+            .push(4)
+            .push(1) // value
+            .push(eoaAddress) // address
+            .push(0) // gas
+            .op(CALL)
+            .op(POP)
+            .push(untrimmedEoaAddress) // address
+            .op(BALANCE)
+            .push(eoaAddress) // address
+            .op(EXTCODEHASH)
+            .compile();
+
+    BytecodeRunner.of(bytecode).run();
+  }
+}

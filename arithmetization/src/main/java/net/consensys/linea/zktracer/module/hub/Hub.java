bbfc78cb3d1afec2cb464ffbf90c54eb6226ef5d
Olivier Bégassat <olivier.begassat.cours@gmail.com>
2024-11-16 13:38:22 +0700

    fix: rely on the HUB's assessment of exceptions
    We (or I) don't really understand when Besu's MessageFrame is
    EXCEPTIONAL_HALT, in particular as it relates to
    
    - invalidCodePrefixException
    - maxCodeSizeException
    
    for RETURN instructions in a deployment context

--- arithmetization/src/main/java/net/consensys/linea/zktracer/module/hub/Hub.java
+++ arithmetization/src/main/java/net/consensys/linea/zktracer/module/hub/Hub.java
@@ -753,7 +753,7 @@ public class Hub implements Module {
     }
 
     if (frame.getDepth() == 0
-        && (frame.getState() == MessageFrame.State.EXCEPTIONAL_HALT || opCode() == REVERT)) {
+        && (isExceptional() || opCode() == REVERT)) {
       this.state.setProcessingPhase(TX_FINL);
       new TxFinalizationSection(this, frame.getWorldUpdater(), true);
     }

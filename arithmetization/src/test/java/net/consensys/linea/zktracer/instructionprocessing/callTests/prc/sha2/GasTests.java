b28e63b598999f8ef13f137bc5cb86f26f447a61
Olivier Bégassat <olivier.begassat.cours@gmail.com>
2024-11-21 14:46:39 +0700

    feat: basic tests for SHA2-256 precompile

--- arithmetization/src/test/java/net/consensys/linea/zktracer/instructionprocessing/callTests/prc/sha2/GasTests.java
+++ arithmetization/src/test/java/net/consensys/linea/zktracer/instructionprocessing/callTests/prc/sha2/GasTests.java
@@ -13,5 +13,37 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 package net.consensys.linea.zktracer.instructionprocessing.callTests.prc.sha2;
+
+import net.consensys.linea.testing.BytecodeCompiler;
+import net.consensys.linea.testing.BytecodeRunner;
+import net.consensys.linea.zktracer.opcode.OpCode;
+import org.hyperledger.besu.datatypes.Address;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
+
+import static net.consensys.linea.zktracer.instructionprocessing.utilities.Calls.appendCall;
+
 public class GasTests {
+
+    @ParameterizedTest
+    @EnumSource(value = OpCode.class, names = {"CALL", "CALLCODE", "DELEGATECALL", "STATICCALL"})
+    void sha2ProvidedWithLittleToNoneGasTest(OpCode callOpCode) {
+
+        BytecodeCompiler program = BytecodeCompiler.newProgram();
+        appendCall(program, callOpCode, 0, Address.fromHexString(Address.SHA256.toHexString()), 1_000_000, 0, 32 * 10, 0, 32);
+
+        BytecodeRunner.of(program).run();
+    }
+
+
+    @ParameterizedTest
+    @EnumSource(value = OpCode.class, names = {"CALL", "CALLCODE", "DELEGATECALL", "STATICCALL"})
+    void sha2ProvidedWithPlentifulGasTest(OpCode callOpCode) {
+
+        BytecodeCompiler program = BytecodeCompiler.newProgram();
+        appendCall(program, callOpCode, 1_000_000, Address.fromHexString(Address.SHA256.toHexString()), 1_000_000, 0, 32 * 10, 0, 32);
+
+        BytecodeRunner.of(program).run();
+    }
 }

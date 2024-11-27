a271c07f4f8d8274591b111ca79b7a375156780d
Olivier Bégassat <olivier.begassat.cours@gmail.com>
2024-11-27 13:23:30 +0700

    feat: root level CREATE tests

--- arithmetization/src/test/java/net/consensys/linea/zktracer/instructionprocessing/createTests/trivial/RootLevel.java
+++ arithmetization/src/test/java/net/consensys/linea/zktracer/instructionprocessing/createTests/trivial/RootLevel.java
@@ -15,16 +15,19 @@
 package net.consensys.linea.zktracer.instructionprocessing.createTests.trivial;
 
 import net.consensys.linea.testing.BytecodeCompiler;
+import net.consensys.linea.testing.BytecodeRunner;
 import net.consensys.linea.zktracer.instructionprocessing.createTests.*;
 import net.consensys.linea.zktracer.opcode.OpCode;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.EnumSource;
 import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Stream;
 
+import static net.consensys.linea.zktracer.instructionprocessing.createTests.CreateType.CREATE2;
 import static net.consensys.linea.zktracer.instructionprocessing.createTests.WhenToTestParameter.BEFORE;
 import static net.consensys.linea.zktracer.instructionprocessing.createTests.WhenToTestParameter.BEFORE_AND_AFTER;
 import static net.consensys.linea.zktracer.opcode.OpCode.*;
@@ -42,18 +45,43 @@ public class RootLevel {
             OffsetParameter offsetParameter,
             boolean revert) {
 
+        BytecodeCompiler program = BytecodeCompiler.newProgram();
+        genericCreate(program, createType, valueParameter, offsetParameter, SizeParameter.ZERO);
+
+        if (revert) {
+            program
+                    .push(0)
+                    .push(0)
+                    .op(REVERT);
+        }
+
+        run(program);
     }
 
-    void rootLevelCreate2AndExtCodeHash(
-            WhenToTestParameter when
-    ) {
+    @ParameterizedTest
+    @EnumSource(WhenToTestParameter.class)
+    void rootLevelCreate2AndExtCodeHash(WhenToTestParameter when) {
 
         int storageKey = 0;
         BytecodeCompiler program = BytecodeCompiler.newProgram();
-        precomputeAndStoreCreate2DeploymentAddress(program, salt01, storageKey);
+        precomputeCreate2DeploymentAddress(program, salt01, storageKey);
+        storeAt(program, storageKey);
 
         if (when == BEFORE || when == BEFORE_AND_AFTER) {
+            loadFromStorage(program, storageKey);
+            program.op(EXTCODEHASH); // we expect to see 0
         }
+
+        genericCreate(program, CREATE2, ValueParameter.ONE, OffsetParameter.ZERO, SizeParameter.ZERO);
+
+        if (when == BEFORE || when == BEFORE_AND_AFTER) {
+            loadFromStorage(program, storageKey);
+            program.op(EQ); // we expect to have the result be true here
+            loadFromStorage(program, storageKey);
+            program.op(EXTCODEHASH); // we expect to see KECCAK(( ))
+        }
+
+        run(program);
     }
 
     private static Stream<Arguments> createParametersForEmptyCreates() {
@@ -74,7 +102,37 @@ public class RootLevel {
         return arguments.stream();
     }
 
-    public static void precomputeAndStoreCreate2DeploymentAddress(BytecodeCompiler program, String salt, int storageKey) {
+    public static void genericCreate(BytecodeCompiler program, CreateType type, ValueParameter valueParameter, OffsetParameter offsetParameter, SizeParameter sizeParameter) {
+
+        switch (sizeParameter) {
+            case ZERO -> program.push(0);
+            case TWELVE -> program.push(12);
+            case THIRTEEN -> program.push(13);
+            case FOURTEEN -> program.push(14);
+            case THIRTY_TWO -> program.push(0x20);
+            case MSIZE -> program.op(MSTORE);
+        }
+        switch (offsetParameter) {
+            case ZERO -> program.push(0);
+            case THREE -> program.push(0);
+            case SIXTEEN -> program.push(0x10);
+            case SIXTEEN_BYTE_INT -> program.push("abe1245ffff123a87000a543eff12aaa");
+            case THIRTY_TWO_BYTE_INT -> program.push("abe1245ffff123a87000a543eff12aaa09987582714266effffefaabc76aa758");
+            case MAX -> program.push("ff".repeat(32));
+        }
+        switch (valueParameter) {
+            case ZERO -> program.push(0);
+            case ONE -> program.push(1);
+            case SELFBALANCE -> program.op(SELFBALANCE);
+            case SELFBALANCE_PLUS_ONE -> program.op(SELFBALANCE).push(1).op(ADD);
+        }
+        switch (type) {
+            case CREATE -> program.op(CREATE);
+            case CREATE2 -> program.op(OpCode.CREATE2);
+        }
+    }
+
+    public static void precomputeCreate2DeploymentAddress(BytecodeCompiler program, String salt, int key) {
         program
                 .push(0xff)
                 .push(0)
@@ -101,8 +159,23 @@ public class RootLevel {
                 .op(SHA3); // extract raw address
         program
                 .push("000000000000000000000000ffffffffffffffffffffffffffffffffffffffff")
-                .op(AND) // computes deployment address
-                .push(storageKey)
-                .op(SSTORE); // SSTORE address at key = 0
+                .op(AND); // computes deployment address
     }
+
+    public static void storeAt(BytecodeCompiler program, int storageKey) {
+        program
+                .push(storageKey)
+                .op(SSTORE);
+    }
+
+    public static void loadFromStorage(BytecodeCompiler program, int storageKey) {
+        program
+                .push(storageKey)
+                .op(SLOAD);
+    }
+
+    private void run(BytecodeCompiler program) {
+        BytecodeRunner.of(program).run();
+    }
+
 }

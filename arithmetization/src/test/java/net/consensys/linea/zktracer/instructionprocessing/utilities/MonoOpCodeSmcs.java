356dc8e43ea97f508df3ac6dbe1365c5e16ce8e1
Olivier Bégassat <olivier.begassat.cours@gmail.com>
2024-11-27 21:40:36 +0700

    fix: make the test work

--- arithmetization/src/test/java/net/consensys/linea/zktracer/instructionprocessing/utilities/MonoOpCodeSmcs.java
+++ arithmetization/src/test/java/net/consensys/linea/zktracer/instructionprocessing/utilities/MonoOpCodeSmcs.java
@@ -29,8 +29,7 @@ public class MonoOpCodeSmcs {
   public static KeyPair keyPair = new SECP256K1().generateKeyPair();
   public static Address userAddress =
       Address.extract(Hash.hash(keyPair.getPublicKey().getEncodedBytes()));
-  public static ToyAccount userAccount =
-      ToyAccount.builder().balance(Wei.fromEth(10)).nonce(99).address(userAddress).build();
+  public static ToyAccount userAccount = ToyAccount.builder().balance(Wei.fromEth(10)).nonce(99).address(userAddress).build();
 
   public static ToyAccount accountWhoseByteCodeIsASingleStop =
       ToyAccount.builder()

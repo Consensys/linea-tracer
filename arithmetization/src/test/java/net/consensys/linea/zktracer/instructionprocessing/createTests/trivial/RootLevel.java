20448957b21466cdf1eedf2ec40b1f9361b523b9
Olivier Bégassat <olivier.begassat.cours@gmail.com>
2024-11-27 13:00:19 +0700

    feat: wip tests

--- arithmetization/src/test/java/net/consensys/linea/zktracer/instructionprocessing/createTests/trivial/RootLevel.java
+++ arithmetization/src/test/java/net/consensys/linea/zktracer/instructionprocessing/createTests/trivial/RootLevel.java
@@ -14,9 +14,95 @@
  */
 package net.consensys.linea.zktracer.instructionprocessing.createTests.trivial;
 
+import net.consensys.linea.testing.BytecodeCompiler;
+import net.consensys.linea.zktracer.instructionprocessing.createTests.*;
+import net.consensys.linea.zktracer.opcode.OpCode;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+
+import static net.consensys.linea.zktracer.instructionprocessing.createTests.WhenToTestParameter.BEFORE;
+import static net.consensys.linea.zktracer.instructionprocessing.createTests.WhenToTestParameter.BEFORE_AND_AFTER;
+import static net.consensys.linea.zktracer.opcode.OpCode.*;
 
 public class RootLevel {
 
+    public String salt01 = "5a1701";
+    public String salt02 = "5a1702";
+
+    @ParameterizedTest
+    @MethodSource("createParametersForEmptyCreates")
+    void rootLevelCreateTests(
+            CreateType createType,
+            ValueParameter valueParameter,
+            OffsetParameter offsetParameter,
+            boolean revert) {
+
+    }
+
+    void rootLevelCreate2AndExtCodeHash(
+            WhenToTestParameter when
+    ) {
+
+        int storageKey = 0;
+        BytecodeCompiler program = BytecodeCompiler.newProgram();
+        precomputeAndStoreCreate2DeploymentAddress(program, salt01, storageKey);
+
+        if (when == BEFORE || when == BEFORE_AND_AFTER) {
+        }
+    }
+
+    private static Stream<Arguments> createParametersForEmptyCreates() {
+
+        final List<ValueParameter> valueParameters = List.of(ValueParameter.ZERO, ValueParameter.ONE);
+
+        List<Arguments> arguments = new ArrayList<>();
+
+        for (CreateType createType : CreateType.values()) {
+            for (ValueParameter valueParameter : valueParameters) {
+                for (OffsetParameter offsetParameter : OffsetParameter.values()) {
+                    arguments.add(Arguments.of(createType, valueParameter, offsetParameter, true));
+                    arguments.add(Arguments.of(createType, valueParameter, offsetParameter, false));
+                }
+            }
+        }
+
+        return arguments.stream();
+    }
+
+    public static void precomputeAndStoreCreate2DeploymentAddress(BytecodeCompiler program, String salt, int storageKey) {
+        program
+                .push(0xff)
+                .push(0)
+                .op(MSTORE8); // (255)
+        program
+                .op(OpCode.ADDRESS)
+                .push(8 * 12)
+                .op(SHL)
+                .push(1)
+                .op(MSTORE);
+        program
+                .push(salt)
+                .push(21)
+                .op(MSTORE); // salt
+        program
+                .push(0)
+                .push(0)
+                .op(SHA3)
+                .push(53)
+                .op(MSTORE); // init code hash = KECCAK(( ))
+        program
+                .push(85) // 1 + 20 + 32 + 32
+                .push(0)
+                .op(SHA3); // extract raw address
+        program
+                .push("000000000000000000000000ffffffffffffffffffffffffffffffffffffffff")
+                .op(AND) // computes deployment address
+                .push(storageKey)
+                .op(SSTORE); // SSTORE address at key = 0
+    }
 }

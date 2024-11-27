7f52891b65724374570a6b7984cafbb7cfae689e
Olivier Bégassat <olivier.begassat.cours@gmail.com>
2024-11-27 19:15:20 +0700

    fix: documentation + no SUX for CREATE2 anymore

--- arithmetization/src/test/java/net/consensys/linea/zktracer/instructionprocessing/createTests/failure/Create2.java
+++ arithmetization/src/test/java/net/consensys/linea/zktracer/instructionprocessing/createTests/failure/Create2.java
@@ -15,9 +15,10 @@
 package net.consensys.linea.zktracer.instructionprocessing.createTests.failure;
 
 import static net.consensys.linea.zktracer.instructionprocessing.createTests.trivial.RootLevel.*;
-import static net.consensys.linea.zktracer.opcode.OpCode.CREATE2;
+import static net.consensys.linea.zktracer.opcode.OpCode.*;
 
 import net.consensys.linea.testing.BytecodeCompiler;
+import org.apache.tuweni.bytes.Bytes;
 import org.junit.jupiter.api.Test;
 
 /** The following tests raise the Failure Condition <b>F</b> CREATE2. */
@@ -42,21 +43,53 @@ public class Create2 {
     run(program);
   }
 
+  /**
+   * We first produce init code that, when run, does
+   *
+   * <p><b>PUSH1 1</b>
+   *
+   * <p><b>PUSH1 0</b>
+   *
+   * <p><b>RETURN</b>
+   *
+   * <p>i.e. 0x60016000f3. We then (1) store that init code in memory (2) use it for a first CREATE2
+   * which deploys a SMC with bytecode of length 1 equal to 0x00 (3) test the (EXT) code size,
+   * code hash and balance of this new account (4) attempt a second deployment at that same
+   * exact address, thus raising the <b>Failure Condition F</b>.
+   */
   @Test
   void failureConditionNonceAndCodeTest() {
 
+    BytecodeCompiler initCode = BytecodeCompiler.newProgram();
+    initCode
+        .push(1) // size
+        .push(0)
+        .op(RETURN);
+    Bytes compiledInitCode = initCode.compile();
+
     BytecodeCompiler program = BytecodeCompiler.newProgram();
     program
+        .push(compiledInitCode)
+        .push(8 * (32 - compiledInitCode.size()))
+        .op(SHL)
+        .push(0)
+        .op(MSTORE); // this puts the init code in memory
+
+    program
         .push(salt01)
+        .push(compiledInitCode.size())
         .push(0)
-        .push(0)
-        .push(1)
-        .op(CREATE2)
+        .push(1) // value
+        .op(CREATE2); // first deployment, deploys monobyte byte code
+
+    program.op(DUP1).op(DUP1).op(EXTCODEHASH).op(EXTCODESIZE).op(BALANCE);
+
+    program
         .push(salt01)
+        .push(compiledInitCode.size())
         .push(0)
-        .push(0)
-        .push(1)
-        .op(CREATE2);
+        .push(1) // value
+        .op(CREATE2); // second (attempted) deployment raises failure condition
 
     run(program);
   }

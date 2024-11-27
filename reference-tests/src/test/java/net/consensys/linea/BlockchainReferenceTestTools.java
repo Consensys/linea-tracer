40b16a0f9cd94b854586ba0b061bd60ab16b0cbc
FlorianHuc <florian.huc@gmail.com>
2024-11-27 18:39:35 +0100

    remove double brackets.

--- reference-tests/src/test/java/net/consensys/linea/BlockchainReferenceTestTools.java
+++ reference-tests/src/test/java/net/consensys/linea/BlockchainReferenceTestTools.java
@@ -120,9 +120,9 @@ public class BlockchainReferenceTestTools {
     PARAMS.ignore("randomStatetest94_\\w+");
 
     // Balance is more than 128 bits
-    PARAMS.ignore(("Call1024PreCalls_d0g0v0_London[London]"));
-    PARAMS.ignore(("Call1024PreCalls_d0g1v0_London[London]"));
-    PARAMS.ignore(("OverflowGasRequire_London[London]"));
+    PARAMS.ignore("Call1024PreCalls_d0g0v0_London[London]");
+    PARAMS.ignore("Call1024PreCalls_d0g1v0_London[London]");
+    PARAMS.ignore("OverflowGasRequire_London[London]");
     PARAMS.ignore("StrangeContractCreation_London[London]");
     PARAMS.ignore("SuicideIssue_London[London]");
     PARAMS.ignore("DelegateCallSpam_London[London]");

b0fd3b61a8eb1f88dab18ccc1d2928444c3ba186
François Bojarski <54240434+letypequividelespoubelles@users.noreply.github.com>
2024-11-28 15:35:03 +0100

    one more

--- reference-tests/src/test/java/net/consensys/linea/BlockchainReferenceTestTools.java
+++ reference-tests/src/test/java/net/consensys/linea/BlockchainReferenceTestTools.java
@@ -138,6 +138,7 @@ public class BlockchainReferenceTestTools {
     PARAMS.ignore("TransactionCollisionToEmptyButNonce_d0g0v1_London\\[London\\]");
     PARAMS.ignore("TransactionCollisionToEmptyButNonce_d0g1v0_London\\[London\\]");
     PARAMS.ignore("TransactionCollisionToEmptyButNonce_d0g1v1_London\\[London\\]");
+    PARAMS.ignore("createJS_ExampleContract_d0g0v0_London\\[London\\]");
 
     // Don't do time-consuming tests.
     PARAMS.ignore("CALLBlake2f_MaxRounds.*");

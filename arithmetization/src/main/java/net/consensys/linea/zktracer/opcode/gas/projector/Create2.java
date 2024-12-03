9cecc1bdba45d2ab2d8ccd22783c237702daa5be
amkCha <29160563+amkCha@users.noreply.github.com>
2024-12-03 17:15:32 +0100

    feat: multi txs for same acccount

--- arithmetization/src/main/java/net/consensys/linea/zktracer/opcode/gas/projector/Create2.java
+++ arithmetization/src/main/java/net/consensys/linea/zktracer/opcode/gas/projector/Create2.java
@@ -15,7 +15,6 @@
 
 package net.consensys.linea.zktracer.opcode.gas.projector;
 
-import static com.google.common.base.Preconditions.checkState;
 import static org.hyperledger.besu.evm.internal.Words.clampedToLong;
 
 import net.consensys.linea.zktracer.module.constants.GlobalConstants;

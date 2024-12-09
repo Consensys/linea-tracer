2b01ffb2071f8a55503ed5d1eb5e56f3ab85e782
Lorenzo Gentile <lorenzogentile404@gmail.com>
2024-12-09 16:44:03 +0100

    feat(blockdata): add draft of ctMax and clean old columns tracing

--- arithmetization/src/main/java/net/consensys/linea/zktracer/module/blockdata/BlockdataOperation.java
+++ arithmetization/src/main/java/net/consensys/linea/zktracer/module/blockdata/BlockdataOperation.java
@@ -15,7 +15,13 @@
 
 package net.consensys.linea.zktracer.module.blockdata;
 
-import static net.consensys.linea.zktracer.module.blockdata.Trace.CT_MAX_FOR_BLOCKDATA;
+import static net.consensys.linea.zktracer.module.blockdata.Trace.CT_MAX_BF;
+import static net.consensys.linea.zktracer.module.blockdata.Trace.CT_MAX_CB;
+import static net.consensys.linea.zktracer.module.blockdata.Trace.CT_MAX_DF;
+import static net.consensys.linea.zktracer.module.blockdata.Trace.CT_MAX_GL;
+import static net.consensys.linea.zktracer.module.blockdata.Trace.CT_MAX_ID;
+import static net.consensys.linea.zktracer.module.blockdata.Trace.CT_MAX_NB;
+import static net.consensys.linea.zktracer.module.blockdata.Trace.CT_MAX_TS;
 import static net.consensys.linea.zktracer.module.constants.GlobalConstants.EVM_INST_BASEFEE;
 import static net.consensys.linea.zktracer.module.constants.GlobalConstants.EVM_INST_CHAINID;
 import static net.consensys.linea.zktracer.module.constants.GlobalConstants.EVM_INST_COINBASE;
@@ -34,6 +40,7 @@ import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.Accessors;
 import net.consensys.linea.zktracer.container.ModuleOperation;
+import net.consensys.linea.zktracer.opcode.OpCode;
 import net.consensys.linea.zktracer.types.UnsignedByte;
 import org.apache.tuweni.bytes.Bytes;
 import org.apache.tuweni.bytes.Bytes32;
@@ -51,12 +58,43 @@ public class BlockdataOperation extends ModuleOperation {
 
   @Override
   protected int computeLineCount() {
-    return CT_MAX_FOR_BLOCKDATA + 1;
+    return ctMax() + 1;
+  }
+
+  private int ctMax() {
+    OpCode opCode =
+        OpCode.COINBASE; // TODO: fill this with the correct opcode, turn it into a global variable
+    switch (opCode) {
+      case COINBASE -> {
+        return CT_MAX_CB;
+      }
+      case TIMESTAMP -> {
+        return CT_MAX_TS;
+      }
+      case NUMBER -> {
+        return CT_MAX_NB;
+      }
+      case DIFFICULTY -> {
+        return CT_MAX_DF;
+      }
+      case GASLIMIT -> {
+        return CT_MAX_GL;
+      }
+      case CHAINID -> {
+        return CT_MAX_ID;
+      }
+      case BASEFEE -> {
+        return CT_MAX_BF;
+      }
+      default -> {
+        throw new IllegalArgumentException("Invalid opcode for BlockdataOperation");
+      }
+    }
   }
 
   public void trace(
       Trace trace, final int relBlock, final long firstBlockNumber, final BigInteger chainId) {
-    for (short ct = 0; ct <= CT_MAX_FOR_BLOCKDATA; ct++) {
+    for (short ct = 0; ct <= ctMax(); ct++) {
       traceBlockConstant(trace, relBlock, firstBlockNumber);
       traceRowDependant(trace, ct, relBlock, chainId);
       trace.validateRow();
@@ -98,44 +136,16 @@ public class BlockdataOperation extends ModuleOperation {
         trace.inst(UnsignedByte.of(EVM_INST_BASEFEE)).wcpFlag(false);
       }
       default -> throw new IllegalArgumentException(
-          String.format("Blockdata max CT is %s, can't write %s", CT_MAX_FOR_BLOCKDATA, ct));
+          String.format("Blockdata max CT is %s, can't write %s", ctMax(), ct));
     }
 
     trace
-        .dataHi(data.slice(0, LLARGE))
-        .dataLo(data.slice(LLARGE, LLARGE))
-        .byteHi0(UnsignedByte.of(data.get(0)))
-        .byteHi1(UnsignedByte.of(data.get(1)))
-        .byteHi2(UnsignedByte.of(data.get(2)))
-        .byteHi3(UnsignedByte.of(data.get(3)))
-        .byteHi4(UnsignedByte.of(data.get(4)))
-        .byteHi5(UnsignedByte.of(data.get(5)))
-        .byteHi6(UnsignedByte.of(data.get(6)))
-        .byteHi7(UnsignedByte.of(data.get(7)))
-        .byteHi8(UnsignedByte.of(data.get(8)))
-        .byteHi9(UnsignedByte.of(data.get(9)))
-        .byteHi10(UnsignedByte.of(data.get(10)))
-        .byteHi11(UnsignedByte.of(data.get(11)))
-        .byteHi12(UnsignedByte.of(data.get(12)))
-        .byteHi13(UnsignedByte.of(data.get(13)))
-        .byteHi14(UnsignedByte.of(data.get(14)))
-        .byteHi15(UnsignedByte.of(data.get(15)))
-        .byteLo0(UnsignedByte.of(data.get(LLARGE + 0)))
-        .byteLo1(UnsignedByte.of(data.get(LLARGE + 1)))
-        .byteLo2(UnsignedByte.of(data.get(LLARGE + 2)))
-        .byteLo3(UnsignedByte.of(data.get(LLARGE + 3)))
-        .byteLo4(UnsignedByte.of(data.get(LLARGE + 4)))
-        .byteLo5(UnsignedByte.of(data.get(LLARGE + 5)))
-        .byteLo6(UnsignedByte.of(data.get(LLARGE + 6)))
-        .byteLo7(UnsignedByte.of(data.get(LLARGE + 7)))
-        .byteLo8(UnsignedByte.of(data.get(LLARGE + 8)))
-        .byteLo9(UnsignedByte.of(data.get(LLARGE + 9)))
-        .byteLo10(UnsignedByte.of(data.get(LLARGE + 10)))
-        .byteLo11(UnsignedByte.of(data.get(LLARGE + 11)))
-        .byteLo12(UnsignedByte.of(data.get(LLARGE + 12)))
-        .byteLo13(UnsignedByte.of(data.get(LLARGE + 13)))
-        .byteLo14(UnsignedByte.of(data.get(LLARGE + 14)))
-        .byteLo15(UnsignedByte.of(data.get(LLARGE + 15)));
+        .iomf(true)
+        .previousConflation(false)
+        // TODO: add missing columns and fill all of them with the correct value
+        //  understand if it makes sense to do the tracing the two different methods or all in one
+        //  spot
+        .validateRow();
   }
 
   private void traceBlockConstant(Trace trace, final int relBlock, final long firstBlockNumber) {

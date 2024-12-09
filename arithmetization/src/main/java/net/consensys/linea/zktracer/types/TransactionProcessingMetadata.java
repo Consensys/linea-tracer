15bbb7869fff77bd7c398be3ed45cc68b2db3d89
Olivier Bégassat <olivier.begassat.cours@gmail.com>
2024-12-09 19:35:22 +0700

    fix: deployment number update for successful SELFDESTRUCT
    we now do the update between the reading of the final state of the
    address and the "wiping" operation

--- arithmetization/src/main/java/net/consensys/linea/zktracer/types/TransactionProcessingMetadata.java
+++ arithmetization/src/main/java/net/consensys/linea/zktracer/types/TransactionProcessingMetadata.java
@@ -180,14 +180,8 @@ public class TransactionProcessingMetadata {
     hubStampTransactionEnd = hub.stamp();
     this.logs = logs;
     for (Address address : selfDestructs) {
-      hub.transients()
-          .conflation()
-          .deploymentInfo()
-          .freshDeploymentNumberFinishingSelfdestruct(
-              address); // depNum += 1 and depStatus <- false
       destructedAccountsSnapshot.add(
-          AccountSnapshot.fromAddress(
-              address, true, hub.deploymentNumberOf(address), hub.deploymentStatusOf(address)));
+          AccountSnapshot.fromAddress(address, true, hub.deploymentNumberOf(address), hub.deploymentStatusOf(address)));
     }
 
     determineSelfDestructTimeStamp();

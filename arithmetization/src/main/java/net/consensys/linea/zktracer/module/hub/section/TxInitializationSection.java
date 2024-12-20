dee2a02785dc6636df6e68a4096396c81a41c8b6
Olivier Bégassat <olivier.begassat.cours@gmail.com>
2024-12-20 10:41:00 +0100

    ras: made isSelfCredit(Hub) a method

--- arithmetization/src/main/java/net/consensys/linea/zktracer/module/hub/section/TxInitializationSection.java
+++ arithmetization/src/main/java/net/consensys/linea/zktracer/module/hub/section/TxInitializationSection.java
@@ -110,7 +110,7 @@ public class TxInitializationSection extends TraceSection implements PostTransac
 
     if (recipientAccount != null) {
       recipientValueReception =
-          isSelfCredit
+          isSelfCredit(hub)
               ? senderValueTransferNew
               : AccountSnapshot.canonical(hub, world, recipientAddress, tx.isRecipientPreWarmed())
                   .setWarmthTo(tx.isRecipientPreWarmed());
@@ -224,5 +224,12 @@ public class TxInitializationSection extends TraceSection implements PostTransac
     }
 
     this.addFragment(initializationContextFragment); // CON i + 5/7
+  }
+  private boolean isSelfCredit(Hub hub) {
+    final TransactionProcessingMetadata tx = hub.txStack().current();
+    final Address senderAddress = tx.getSender();
+    final Address recipientAddress = tx.getEffectiveRecipient();
+
+    return recipientAddress.equals(senderAddress);
   }
 }

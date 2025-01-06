5df80276c9346d3cc4cbad7df05f6e5f5d734fd4
Olivier Bégassat <olivier.begassat.cours@gmail.com>
2025-01-06 19:12:13 +0100

    fix: the "1th value 256 does not fit a byte" issue

--- arithmetization/src/main/java/net/consensys/linea/zktracer/module/hub/fragment/imc/oob/precompiles/ModexpPricingOobCall.java
+++ arithmetization/src/main/java/net/consensys/linea/zktracer/module/hub/fragment/imc/oob/precompiles/ModexpPricingOobCall.java
@@ -55,7 +55,7 @@ public class ModexpPricingOobCall extends OobCall {
         .data4(booleanToBytes(ramSuccess))
         .data5(bigIntegerToBytes(returnGas))
         .data6(bigIntegerToBytes(exponentLog))
-        .data7(Bytes.of(maxMbsBbs))
+        .data7(bigIntegerToBytes(BigInteger.valueOf(maxMbsBbs)))
         .data8(booleanToBytes(returnAtCapacityNonZero))
         .data9(ZERO);
   }
@@ -71,7 +71,7 @@ public class ModexpPricingOobCall extends OobCall {
         .pMiscOobData4(booleanToBytes(ramSuccess))
         .pMiscOobData5(bigIntegerToBytes(returnGas))
         .pMiscOobData6(bigIntegerToBytes(exponentLog))
-        .pMiscOobData7(Bytes.of(maxMbsBbs))
+        .pMiscOobData7(bigIntegerToBytes(BigInteger.valueOf(maxMbsBbs)))
         .pMiscOobData8(booleanToBytes(returnAtCapacityNonZero))
         .pMiscOobData9(ZERO);
   }

e4f4725272d06d4b7e391e3259b707aee1226cbc
Fabio Di Fabio <fabio.difabio@consensys.net>
2025-01-22 15:54:05 +0100

    Update arithmetization/src/main/java/net/consensys/linea/blockcapture/reapers/Reaper.java
    Removing resolved comment

--- arithmetization/src/main/java/net/consensys/linea/blockcapture/reapers/Reaper.java
+++ arithmetization/src/main/java/net/consensys/linea/blockcapture/reapers/Reaper.java
@@ -73,7 +73,7 @@ public class Reaper {
       final BlockHeader header, final BlockBody body, final Address miningBeneficiary) {
     this.blocks.add(
         BlockSnapshot.of((org.hyperledger.besu.ethereum.core.BlockHeader) header, body));
-    this.conflationAddresses.touch(miningBeneficiary); // ToDo: replace with miningBeneficiary?
+    this.conflationAddresses.touch(miningBeneficiary);
     txIndex = 0; // reset
   }
 

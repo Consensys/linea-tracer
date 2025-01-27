f83c1c28f43090d34220d24bc1d0604a8b9ff3fa
Olivier Bégassat <olivier.begassat.cours@gmail.com>
2025-01-27 21:01:54 +0100

    fix: pre-wiping snapshot now uses canonical()
    fixes issue where SELFDESTRUCT's would have old and new
    AccountSnapshot's be empty on the account wiping account-row.

--- arithmetization/src/main/java/net/consensys/linea/zktracer/types/TransactionProcessingMetadata.java
+++ arithmetization/src/main/java/net/consensys/linea/zktracer/types/TransactionProcessingMetadata.java
@@ -181,8 +181,7 @@ public class TransactionProcessingMetadata {
     this.logs = logs;
     for (Address address : selfDestructs) {
       destructedAccountsSnapshot.add(
-          AccountSnapshot.fromAddress(
-              address, true, hub.deploymentNumberOf(address), hub.deploymentStatusOf(address)));
+          AccountSnapshot.canonical(hub, address));
     }
 
     determineSelfDestructTimeStamp();

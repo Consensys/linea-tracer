845858966ec4c2d86ce5022e65b5e44e2e70d6ee
Lorenzo Gentile <lorenzogentile404@gmail.com>
2025-01-28 17:11:19 +0100

    fixed testing contract

--- arithmetization/src/test/resources/contracts/sloadAndSstore/ContractForTestingSLoadAndSStore.sol
+++ arithmetization/src/test/resources/contracts/sloadAndSstore/ContractForTestingSLoadAndSStore.sol
@@ -2,38 +2,40 @@
 pragma solidity ^0.8.0;
 
 contract ContractForTestingSLoadAndSStore {
-    uint256 public counter;
+    uint256 public counter = 0;
+    uint256 public constant COUNTER_THRESHOLD_FOR_REVERT = 5;
+    address public nextInstanceAddress;
+    event CounterUpdated(uint256 counter);
 
-    // Threshold for the counter
-    uint256 public constant THRESHOLD = 4;
-
-    // Address of another instance of this contract
-    address public otherContract;
-
-    /**
-     * @notice Constructor to set the address of the other contract instance.
-     * @param _otherContract Address of another instance of this contract.
-     */
-    constructor(address _otherContract) {
-        // 0x0000000000000000000000000000000000000000 for the first instantiation
-        otherContract = _otherContract;
+    constructor(address _nextInstanceAddress) {
+        // Provide
+        // 0x0000000000000000000000000000000000000000 for instantiating E
+        // address of E for instantiating D
+        // address of D for instantiating C
+        // address of C for instantiating B
+        // address of B for instantiating A
+        nextInstanceAddress = _nextInstanceAddress;
     }
 
-    /**
-     * @notice Increment the counter and call the same function on another instance of this contract.
-     * @param _counter The current counter value.
-     */
     function incrementAndCall(uint256 _counter) external {
-         // Increment the counter
+        // Increment the counter
         counter = _counter + 1;
-
-        // Ensure the counter does not exceed the threshold
-        require(counter < THRESHOLD, "Counter value exceeds threshold");
-
-
-        // Call the same function on the other instance of the contract
-        if (otherContract != address(0)) {
-            ContractForTestingSLoadAndSStore(otherContract).incrementAndCall(counter);
+        // Emit event with the value of the counter
+        emit CounterUpdated(counter);
+        // Revert in case counter reaches the threshold
+        require(counter < COUNTER_THRESHOLD_FOR_REVERT, "counter reached COUNTER_THRESHOLD_FOR_REVERT");
+        // Invoke same function of the contract at nextInstanceAddress
+        if (nextInstanceAddress != address(0)) {
+            ContractForTestingSLoadAndSStore(nextInstanceAddress).incrementAndCall(counter);
         }
     }
 }
+
+/*
+* EOA A.incrementAndCall(0) 
+* A. 1 < 5 -> B.incrementAndCall(1)
+* B. 2 < 5 -> C.incrementAndCall(2)
+* C. 3 < 5 -> D.incrementAndCall(3)
+* D. 4 < 5 -> E.incrementAndCall(4)
+* E. 5 = 5 -> REVERT
+*/

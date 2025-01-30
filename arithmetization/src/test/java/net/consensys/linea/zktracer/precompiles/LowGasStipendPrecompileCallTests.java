6abb938f66d336ccdf5a833aba00e9d9938c3aa6
Lorenzo Gentile <lorenzogentile404@gmail.com>
2025-01-30 17:07:55 +0100

    fixed some stuff related to BLAKE2F

--- arithmetization/src/test/java/net/consensys/linea/zktracer/precompiles/LowGasStipendPrecompileCallTests.java
+++ arithmetization/src/test/java/net/consensys/linea/zktracer/precompiles/LowGasStipendPrecompileCallTests.java
@@ -1,5 +1,8 @@
 package net.consensys.linea.zktracer.precompiles;
 
+import static net.consensys.linea.zktracer.module.blake2fmodexpdata.BlakeModexpDataOperation.BLAKE2f_HASH_OUTPUT_SIZE;
+import static org.hyperledger.besu.datatypes.Address.ALTBN128_PAIRING;
+import static org.hyperledger.besu.datatypes.Address.BLAKE2B_F_COMPRESSION;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.ArrayList;
@@ -22,21 +25,36 @@ public class LowGasStipendPrecompileCallTests {
   @ParameterizedTest
   @MethodSource("lowGasStipendPrecompileCallTestSource")
   void lowGasStipendPrecompileCallTest(Address precompileAddress, boolean isZeroArgument) {
-    final Bytes bytecode =
-        BytecodeCompiler.newProgram()
-            .push(isZeroArgument ? 0 : 1) // retSize
-            .push(isZeroArgument ? 0 : 1) // retOffset
-            .push(
-                isZeroArgument
-                    ? 0
-                    : (precompileAddress == Address.ALTBN128_PAIRING ? 192 : 1)) // argsSize
-            .push(isZeroArgument ? 0 : 1) // argsOffset
-            .push(0) // value
-            .push(precompileAddress) // address
-            .push(1) // gas, that is deliberately insufficient
-            .op(OpCode.CALL)
-            .compile();
-    final BytecodeRunner bytecodeRunner = BytecodeRunner.of(bytecode);
+    int argsSize = 1;
+    if (precompileAddress == ALTBN128_PAIRING) {
+      argsSize = 192;
+    } else if (precompileAddress == BLAKE2B_F_COMPRESSION) {
+      argsSize = 213;
+    } else {
+      argsSize = isZeroArgument ? 0 : 1;
+    }
+
+    final BytecodeCompiler program = BytecodeCompiler.newProgram();
+
+    if (precompileAddress == BLAKE2B_F_COMPRESSION) {
+      // TODO: do we need to set the retSize to the correct value? Look at blakeTests? Do we need pass all arguments?
+      program
+          .push(10) // value = r for Blake call
+          .push(isZeroArgument ? 0 : 1) // offset
+          .op(OpCode.MSTORE8);
+    }
+
+    program
+        .push(precompileAddress == Address.BLAKE2B_F_COMPRESSION ? BLAKE2f_HASH_OUTPUT_SIZE : (isZeroArgument ? 0 : 1)) // retSize
+        .push(isZeroArgument ? 0 : 1) // retOffset
+        .push(isZeroArgument ? 0 : argsSize) // argsSize
+        .push(isZeroArgument ? 0 : 1) // argsOffset
+        .push(0) // value
+        .push(precompileAddress) // address
+        .push(1) // gas, that is deliberately insufficient
+        .op(OpCode.CALL)
+        .compile();
+    final BytecodeRunner bytecodeRunner = BytecodeRunner.of(program);
     bytecodeRunner.run(1_000_000L); // huge gas limit
     final Hub hub = bytecodeRunner.getHub();
     // Operation with index 1 is the processing of the precompile contract
@@ -60,7 +78,7 @@ public class LowGasStipendPrecompileCallTests {
       arguments.add(Arguments.of(Address.ALTBN128_ADD, isZeroArgument));
       arguments.add(Arguments.of(Address.ALTBN128_MUL, isZeroArgument));
       arguments.add(Arguments.of(Address.ALTBN128_PAIRING, isZeroArgument));
-      arguments.add(Arguments.of(Address.BLAKE2B_F_COMPRESSION, isZeroArgument));
+      arguments.add(Arguments.of(BLAKE2B_F_COMPRESSION, isZeroArgument));
     }
     return arguments.stream();
   }

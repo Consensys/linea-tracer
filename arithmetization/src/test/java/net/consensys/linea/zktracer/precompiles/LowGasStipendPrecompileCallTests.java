ac99bab283ef9adfc61194e770745a518c811254
Lorenzo Gentile <lorenzogentile404@gmail.com>
2025-02-11 20:33:36 +0100

    partially fixed test logic

--- arithmetization/src/test/java/net/consensys/linea/zktracer/precompiles/LowGasStipendPrecompileCallTests.java
+++ arithmetization/src/test/java/net/consensys/linea/zktracer/precompiles/LowGasStipendPrecompileCallTests.java
@@ -15,6 +15,8 @@
 
 package net.consensys.linea.zktracer.precompiles;
 
+import static net.consensys.linea.zktracer.module.constants.GlobalConstants.WORD_SIZE;
+import static net.consensys.linea.zktracer.opcode.OpCode.MSTORE;
 import static org.hyperledger.besu.datatypes.Address.ALTBN128_ADD;
 import static org.hyperledger.besu.datatypes.Address.ALTBN128_MUL;
 import static org.hyperledger.besu.datatypes.Address.ALTBN128_PAIRING;
@@ -69,24 +71,37 @@ public class LowGasStipendPrecompileCallTests {
     final BytecodeCompiler program = BytecodeCompiler.newProgram();
 
     // In order to actually trigger the insufficient we need to:
-    // - Set a specific args size from BLAKE2F AND EC_PAIRING
+    // - Set a specific args size for BLAKE2F and EC_PAIRING
     // - Set the r value of BLAKE2F to something greater than the gas stipend
-    int argsSize;
+    final int value = valueParameter.isZeroArgument() ? 0 : 1;
+    final int argsSize; // depends on the called precompile
+    final int argsOffset = valueParameter.isZeroArgument() ? 0 : 1;
+    final int retSize = valueParameter.isZeroArgument() ? 0 : 1;
+    final int retOffset = valueParameter.isZeroArgument() ? 0 : 1;
+
+    // BLAKE2F specific parameters
     int rFirstByte = valueParameter.isZeroArgument() ? 0 : 0x12;
-    int r = rFirstByte * (1 << 24);
+    int r = rFirstByte * (1 << 24); // as r is 4 bytes, rFirstByte is right padded
     if (precompileAddress == BLAKE2B_F_COMPRESSION) {
       program
-          .push(rFirstByte) // rFirstByte (as r is 4 bytes, rFirstByte is padded to 0x12000000)
+          .push(rFirstByte) // For simplicity, we only set the first byte of r
           .push(valueParameter.isZeroArgument() ? 0 : 1) // offset
           .op(OpCode.MSTORE8);
       argsSize = 213;
     } else if (precompileAddress == ALTBN128_PAIRING) {
       argsSize = 192;
+    } else if ((precompileAddress == SHA256
+            || precompileAddress == RIPEMD160
+            || precompileAddress == ID)
+        && !valueParameter.isZeroArgument()) {
+      int nWords = 1024;
+      argsSize = nWords * WORD_SIZE; // This guarantees that precompileCost > gasBonus
+      populateMemory(program, nWords, argsOffset);
     } else {
       argsSize = valueParameter.isZeroArgument() ? 0 : 1;
     }
 
-    int precompileCost = 0;
+    final int precompileCost;
     if (precompileAddress.equals(ECREC)) {
       precompileCost = 3000;
     } else if (precompileAddress.equals(SHA256)) {
@@ -118,17 +133,30 @@ public class LowGasStipendPrecompileCallTests {
           case COST_PLUS_ONE -> precompileCost + 1;
         };
 
-    // In case funds are sent to the precompile contract, a gas bonus of 2300 is added
-    int gasBonus = valueParameter.isZeroArgument() ? 0 : 2300;
-    gas += gasBonus;
+    // In case funds are sent to the precompile contract (valueParameter == NON_ZERO)
+    // a gas bonus of 2300 is added to the transaction (gas stipend).
+    // We now deduce that gas bonus from the gas stipend to trigger the
+    // insufficient gas for the precompile call in the non-trivial cases (COST_MINUS_ONE, COST,
+    // COST_PLUS_ONE).
+    // Note that we exclude the case of MODEXP as it is treated in a separate test
+    // and the case of ALTBN128_ADD as it has a fixed gas cost of 150.
+    if (!valueParameter.isZeroArgument()
+        && (gasParameter == GasParameter.COST_MINUS_ONE
+            || gasParameter == GasParameter.COST
+            || gasParameter == GasParameter.COST_PLUS_ONE)
+        && !precompileAddress.equals(ALTBN128_ADD)
+        && !precompileAddress.equals(MODEXP)) {
+      final int gasBonus = 2300;
+      gas -= gasBonus;
+    }
 
     // Common program for all precompile calls
     program
-        .push(valueParameter.isZeroArgument() ? 0 : 1) // retSize
-        .push(valueParameter.isZeroArgument() ? 0 : 1) // retOffset
+        .push(retSize) // retSize
+        .push(retOffset) // retOffset
         .push(argsSize) // argsSize
-        .push(valueParameter.isZeroArgument() ? 0 : 1) // argsOffset
-        .push(valueParameter.isZeroArgument() ? 0 : 1) // value
+        .push(argsOffset) // argsOffset
+        .push(value) // value
         .push(precompileAddress) // address
         .push(gas) // gas
         .op(OpCode.CALL)
@@ -137,8 +165,8 @@ public class LowGasStipendPrecompileCallTests {
     bytecodeRunner.run(1_000_000L); // huge gas limit
     final Hub hub = bytecodeRunner.getHub();
 
-    // Here we check if OOB detects the insufficient gas for the precompile call
-    // As the number of OOB operation required is variable, we iterate over all the operations
+    // Here we check if OOB detects the insufficient gas for the precompile call.
+    // As the number of OOB operation required is variable, we iterate over all the operations.
     boolean insufficientGasForPrecompile = false;
     BigInteger actualPrecompileCost = BigInteger.ZERO;
     for (int i = 0; i < hub.oob().operations().size(); i++) {
@@ -150,16 +178,16 @@ public class LowGasStipendPrecompileCallTests {
       }
     }
 
+    // We assert that the precompileCost we compute here is the same as the one computed in OOB
     assertEquals(BigInteger.valueOf(precompileCost), actualPrecompileCost);
 
-    if (valueParameter.isZeroArgument()) {
-      if (gasParameter == GasParameter.COST
-          || gasParameter == GasParameter.COST_PLUS_ONE
-          || (precompileAddress.equals(BLAKE2B_F_COMPRESSION) && r == 0)) {
-        assertFalse(insufficientGasForPrecompile);
-      } else {
-        assertTrue(insufficientGasForPrecompile);
-      }
+    // We assert that the insufficientGasForPrecompile flag is set correctly
+    if (gasParameter == GasParameter.COST
+        || gasParameter == GasParameter.COST_PLUS_ONE
+        || (precompileAddress.equals(BLAKE2B_F_COMPRESSION) && r == 0)) {
+      assertFalse(insufficientGasForPrecompile);
+    } else {
+      assertTrue(insufficientGasForPrecompile);
     }
   }
 
@@ -172,7 +200,7 @@ public class LowGasStipendPrecompileCallTests {
         arguments.add(Arguments.of(RIPEMD160, valueParameter, gasParameter));
         arguments.add(Arguments.of(ID, valueParameter, gasParameter));
         if (valueParameter == ValueParameter.ZERO) {
-          // The NON_ZERO case will be treated in a separate test
+          // The NON_ZERO for MODEXP case will be treated in a separate test
           arguments.add(Arguments.of(MODEXP, valueParameter, gasParameter));
         }
         arguments.add(Arguments.of(ALTBN128_ADD, valueParameter, gasParameter));
@@ -184,5 +212,26 @@ public class LowGasStipendPrecompileCallTests {
     // arguments.clear();
     // arguments.add(Arguments.of(RIPEMD160, ValueParameter.NON_ZERO, GasParameter.ZERO));
     return arguments.stream();
+  }
+
+  // TODO: do not replicate this method
+  //  use the one in
+  // arithmetization/src/test/java/net/consensys/linea/zktracer/instructionprocessing/callTests/Utilities.java
+  //  after merge
+  /**
+   * {@link #populateMemory} populates memory with <b>nWords</b> chosen cyclically from the set of 6
+   * EVM words obtained by repeating the strings <b>aa</b>, <b>bb</b>, ..., <b>ff</b> 32 times.
+   *
+   * @param program
+   * @param nWords
+   */
+  public static void populateMemory(BytecodeCompiler program, int nWords, int offset) {
+    List<String> abcdef = List.of("aa", "bb", "cc", "dd", "ee", "ff");
+    for (int i = 0; i < nWords; i++) {
+      program
+          .push(abcdef.get(i % abcdef.size()).repeat(WORD_SIZE)) // value, a 32 byte word
+          .push(offset + i * WORD_SIZE) // offset
+          .op(MSTORE);
+    }
   }
 }

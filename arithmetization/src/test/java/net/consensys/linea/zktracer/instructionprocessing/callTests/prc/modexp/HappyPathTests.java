5dfec53b9e2cf908527bf13f3c9bbb3cca52b27a
Olivier Bégassat <olivier.begassat.cours@gmail.com>
2025-02-13 12:20:45 +0100

    ras

--- arithmetization/src/test/java/net/consensys/linea/zktracer/instructionprocessing/callTests/prc/modexp/HappyPathTests.java
+++ arithmetization/src/test/java/net/consensys/linea/zktracer/instructionprocessing/callTests/prc/modexp/HappyPathTests.java
@@ -30,7 +30,7 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
-// @Tag("weekly")
+@Tag("weekly")
 public class HappyPathTests {
 
   private final boolean variant1 = true;

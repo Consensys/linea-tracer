7721dc1aaa97820a12d4f694681973f485b4abc7
Olivier Bégassat <olivier.begassat.cours@gmail.com>
2025-02-18 21:21:06 +0100

    ras: disable IllegalModexpTest (which is known to fail)

--- arithmetization/src/test/java/net/consensys/linea/replaytests/IllegalModexpTest.java
+++ arithmetization/src/test/java/net/consensys/linea/replaytests/IllegalModexpTest.java
@@ -18,10 +18,12 @@ import static net.consensys.linea.replaytests.ReplayTestTools.replay;
 import static net.consensys.linea.testing.ReplayExecutionEnvironment.LINEA_MAINNET;
 
 import net.consensys.linea.UnitTestWatcher;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 
+@Disabled
 @Tag("replay")
 @ExtendWith(UnitTestWatcher.class)
 public class IllegalModexpTest {

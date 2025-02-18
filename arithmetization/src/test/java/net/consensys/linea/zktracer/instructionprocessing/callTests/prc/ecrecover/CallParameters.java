0fcc291e620ec44c36b9d4148bd07cf6ce3c8b2a
Olivier Bégassat <olivier.begassat.cours@gmail.com>
2025-02-18 19:45:08 +0100

    feat: progress on ECRECOVER

---
+++ arithmetization/src/test/java/net/consensys/linea/zktracer/instructionprocessing/callTests/prc/ecrecover/CallParameters.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright Consensys Software Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+package net.consensys.linea.zktracer.instructionprocessing.callTests.prc.ecrecover;
+
+import net.consensys.linea.zktracer.instructionprocessing.callTests.prc.GasParameter;
+import net.consensys.linea.zktracer.instructionprocessing.callTests.prc.ReturnAtParameter;
+import net.consensys.linea.zktracer.opcode.OpCode;
+
+public class CallParameters {
+
+    public final OpCode call;
+    public final GasParameter gas;
+    public final MemoryContentsParameter memoryContent;
+    public final CallDataSizeParameter cds;
+    public final ReturnAtParameter returnAt;
+    public final boolean willRevert;
+
+    public CallParameters(
+            OpCode call,
+            GasParameter gas,
+            MemoryContentsParameter memoryContent,
+            CallDataSizeParameter cds,
+            ReturnAtParameter returnAt,
+            boolean willRevert) {
+        this.call = call;
+        this.gas = gas;
+        this.memoryContent = memoryContent;
+        this.cds = cds;
+        this.returnAt = returnAt;
+        this.willRevert = willRevert;
+    }
+}
---
+++ arithmetization/src/test/java/net/consensys/linea/zktracer/instructionprocessing/callTests/prc/ecrecover/HappyPathTests.java
@@ -0,0 +1,67 @@
+/*
+ * Copyright Consensys Software Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+package net.consensys.linea.zktracer.instructionprocessing.callTests.prc.ecrecover;
+
+import net.consensys.linea.testing.BytecodeCompiler;
+import org.hyperledger.besu.datatypes.Address;
+
+import static net.consensys.linea.zktracer.module.constants.GlobalConstants.WORD_SIZE;
+import static net.consensys.linea.zktracer.opcode.OpCode.*;
+
+public class HappyPathTests {
+
+    /** Constructs a call to the <b>ECADD</b> precompile in terms of {@link CallParameters}. */
+    public void appendHappyPathPrecompileCall(BytecodeCompiler program, CallParameters params) {
+
+        // push r@c onto the stack
+        switch (params.returnAt) {
+            case EMPTY -> program.push(0);
+            case PARTIAL -> program.push(12 + 6); // the first 12 bytes are zeros for successful ECRECOVER calls
+            case FULL -> program.push(WORD_SIZE);
+        }
+
+        // push the r@o onto the stack
+        program.push(4 * WORD_SIZE);
+
+        // push the cds onto the stack
+        switch (params.cds) {
+            case EMPTY -> program.push(0);
+            case MISSING_FINAL_BYTE_OF_R -> program.push(3 * WORD_SIZE - 1);
+            case MISSING_FINAL_BYTE_OF_S -> program.push(4 * WORD_SIZE - 1);
+            case FULL -> program.op(MSIZE);
+        }
+
+        // push the cdo onto the stack;
+        program.push(0);
+
+        // if appropriate, push the value onto the stack
+        if (params.call.callHasValueArgument()) {
+            program.push(0x0600);
+        }
+
+        program.push(Address.ECREC);
+
+        // push gas onto the stack
+        int callStipend = params.call.callHasValueArgument() ? 2_300 : 0;
+        switch (params.gas) {
+            case ZERO -> program.push(0); // interesting in the nonzero value case
+            case COST_MO -> program.push(3000 - callStipend - 1);
+            case COST -> program.push(3000 - callStipend);
+            case FULL -> program.op(GAS);
+            default -> throw new RuntimeException("Unsupported gas parameter");
+        }
+        program.op(params.call);
+    }
+}

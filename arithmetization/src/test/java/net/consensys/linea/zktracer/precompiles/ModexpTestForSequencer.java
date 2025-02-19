ee7fd24eed396f728643e1f755802ba6a9239ec9
Lorenzo Gentile <lorenzogentile404@gmail.com>
2025-02-19 20:04:22 +0100

    added test cases

--- arithmetization/src/test/java/net/consensys/linea/zktracer/precompiles/ModexpTestForSequencer.java
+++ arithmetization/src/test/java/net/consensys/linea/zktracer/precompiles/ModexpTestForSequencer.java
@@ -15,8 +15,12 @@
 
 package net.consensys.linea.zktracer.precompiles;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
+import java.util.stream.Stream;
 
+import lombok.Getter;
 import net.consensys.linea.testing.ToyAccount;
 import net.consensys.linea.testing.ToyExecutionEnvironmentV2;
 import net.consensys.linea.testing.ToyTransaction;
@@ -30,12 +34,18 @@ import org.hyperledger.besu.datatypes.Hash;
 import org.hyperledger.besu.datatypes.TransactionType;
 import org.hyperledger.besu.datatypes.Wei;
 import org.hyperledger.besu.ethereum.core.Transaction;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class ModexpTestForSequencer {
+  Random RANDOM = new Random(123);
 
-  @Test
-  void modexpTestForSequencer() {
+  @ParameterizedTest
+  @MethodSource("modexpTestForSequencerSource")
+  void modexpTestForSequencer(Bytes input) {
     // arithmetization/src/test/resources/contracts/modexp/ModexpContract.sol
     // solc-select use 0.8.20
     // solc *.sol --bin-runtime --evm-version london -o compiledContracts
@@ -56,16 +66,19 @@ public class ModexpTestForSequencer {
                     "608060405234801561001057600080fd5b506004361061002b5760003560e01c8063bdabdbbd14610030575b600080fd5b61004a60048036038101906100459190610232565b610060565b60405161005791906102fa565b60405180910390f35b60606000825190506000602067ffffffffffffffff81111561008557610084610107565b5b6040519080825280601f01601f1916602001820160405280156100b75781602001600182028036833780820191505090505b5090506020840160208201600081858460055afa5050508092505050919050565b6000604051905090565b600080fd5b600080fd5b600080fd5b600080fd5b6000601f19601f8301169050919050565b7f4e487b7100000000000000000000000000000000000000000000000000000000600052604160045260246000fd5b61013f826100f6565b810181811067ffffffffffffffff8211171561015e5761015d610107565b5b80604052505050565b60006101716100d8565b905061017d8282610136565b919050565b600067ffffffffffffffff82111561019d5761019c610107565b5b6101a6826100f6565b9050602081019050919050565b82818337600083830152505050565b60006101d56101d084610182565b610167565b9050828152602081018484840111156101f1576101f06100f1565b5b6101fc8482856101b3565b509392505050565b600082601f830112610219576102186100ec565b5b81356102298482602086016101c2565b91505092915050565b600060208284031215610248576102476100e2565b5b600082013567ffffffffffffffff811115610266576102656100e7565b5b61027284828501610204565b91505092915050565b600081519050919050565b600082825260208201905092915050565b60005b838110156102b557808201518184015260208101905061029a565b60008484015250505050565b60006102cc8261027b565b6102d68185610286565b93506102e6818560208601610297565b6102ef816100f6565b840191505092915050565b6000602082019050818103600083015261031481846102c1565b90509291505056fea2646970667358221220180bbc5fd440cd7487f3c2d407b6c825ea7fbac21f31e09b956884b1fa2f2c0164736f6c63430008140033"))
             .build();
 
-    Bytes input =
-        Bytes.fromHexString(
-            "000000000000000000000000000000000000000000000000000000000000000100000000000000000000000000000000000000000000000000000000000000010000000000000000000000000000000000000000000000000000000000000001aabbcc");
     Bytes32 size = Bytes32.leftPad(Bytes.ofUnsignedInt(input.size()));
 
     Transaction txToModexpContractAccount =
         ToyTransaction.builder()
             .sender(userAccount)
             .to(modexpContractAccount)
-            .payload(Bytes.concatenate(Bytes.fromHexString("0xbdabdbbd" + "0000000000000000000000000000000000000000000000000000000000000020"), size, input)) // function selector, offset (always 0x20), size, input
+            .payload(
+                Bytes.concatenate(
+                    Bytes.fromHexString(
+                        "0xbdabdbbd"
+                            + "0000000000000000000000000000000000000000000000000000000000000020"),
+                    size,
+                    input)) // function selector, offset (always 0x20), size, input
             .transactionType(TransactionType.FRONTIER)
             .value(Wei.ZERO)
             .keyPair(keyPair)
@@ -82,5 +95,56 @@ public class ModexpTestForSequencer {
             .build();
 
     toyExecutionEnvironmentV2.run();
+  }
+
+  private Stream<Arguments> modexpTestForSequencerSource() {
+    List<Arguments> arguments = new ArrayList<>();
+    for (ModexpCallDataWordVariants w1 : ModexpCallDataWordVariants.values()) {
+      arguments.add(Arguments.of(w1.getW()));
+      for (ModexpCallDataWordVariants w2 : ModexpCallDataWordVariants.values()) {
+        arguments.add(Arguments.of(Bytes.concatenate(w1.getW(), w2.getW())));
+        for (ModexpCallDataWordVariants w3 : ModexpCallDataWordVariants.values()) {
+          arguments.add(Arguments.of(Bytes.concatenate(w1.getW(), w2.getW(), w3.getW())));
+          arguments.add(
+              Arguments.of(
+                  Bytes.concatenate(w1.getW(), w2.getW(), w3.getW(), Bytes.random(1536, RANDOM))));
+        }
+      }
+    }
+    return arguments.stream();
+  }
+
+  @Getter
+  public enum ModexpCallDataWordVariants {
+    EMPTY(Bytes.fromHexString("")),
+    ZERO(Bytes.fromHexString("0000000000000000000000000000000000000000000000000000000000000000")),
+    RAND(Bytes.fromHexString("000000000000000000000000000000000000000000000000000000000000013f")),
+    MAX_LEGAL_FULL(
+        Bytes.fromHexString("0000000000000000000000000000000000000000000000000000000000000200")),
+    MAX_LEGAL_SHORT(
+        Bytes.fromHexString("00000000000000000000000000000000000000000000000000000000000002")),
+    MIN_ILLEGAL_FULL(
+        Bytes.fromHexString(
+            "0000000000000000000000000000000000000000000000000000000000000201")), // invalid
+    MIN_ILLEGAL_SHORT(
+        Bytes.fromHexString(
+            "00000000000000000000000000000000000000000000000000000000000003")), // invalid
+    ILLEGAL_SHORTEST(Bytes.fromHexString("ff")), // invalid
+    MAX_WORD(
+        Bytes.fromHexString(
+            "ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff")); // invalid
+
+    private final Bytes w;
+
+    ModexpCallDataWordVariants(Bytes w) {
+      this.w = w;
+    }
+
+    public boolean isInvalid() {
+      return this.getW() == MIN_ILLEGAL_FULL.getW()
+          || this.getW() == MIN_ILLEGAL_SHORT.getW()
+          || this.getW() == ILLEGAL_SHORTEST.getW()
+          || this.getW() == MAX_WORD.getW();
+    }
   }
 }

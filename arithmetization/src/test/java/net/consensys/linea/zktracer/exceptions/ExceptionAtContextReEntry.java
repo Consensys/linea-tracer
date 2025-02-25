24adb0a248621c810ef9777d01ceb911157c26a7
Olivier Bégassat <olivier.begassat.cours@gmail.com>
2025-02-25 12:10:22 +0100

    spotless

--- arithmetization/src/test/java/net/consensys/linea/zktracer/exceptions/ExceptionAtContextReEntry.java
+++ arithmetization/src/test/java/net/consensys/linea/zktracer/exceptions/ExceptionAtContextReEntry.java
@@ -14,188 +14,161 @@
  */
 package net.consensys.linea.zktracer.exceptions;
 
-import net.consensys.linea.testing.BytecodeCompiler;
-import net.consensys.linea.testing.BytecodeRunner;
-import net.consensys.linea.zktracer.module.hub.section.TraceSection;
-import org.apache.tuweni.bytes.Bytes;
-import org.hyperledger.besu.evm.frame.MessageFrame;
-import org.junit.jupiter.api.Test;
-import net.consensys.linea.zktracer.module.hub.Hub;
-
 import static net.consensys.linea.zktracer.module.constants.GlobalConstants.WORD_SIZE;
 import static net.consensys.linea.zktracer.opcode.OpCode.*;
+
+import net.consensys.linea.testing.BytecodeCompiler;
+import net.consensys.linea.testing.BytecodeRunner;
+import net.consensys.linea.zktracer.module.hub.Hub;
+import org.apache.tuweni.bytes.Bytes;
+import org.junit.jupiter.api.Test;
 
 /**
  * We explore what happens if the first instruction after resuming the execution of a frame is
  * exceptional. We also explore what happens, though this ought not to be an issue, what happens
- * when resuming the execution of the parent frame after an exception in the child frame.
- * This tests whether {@link Hub#unlatchStack} behaves appropriately for <b>CALL</b>-type and <b>CREATE</b>-type instructions,
- * when either
+ * when resuming the execution of the parent frame after an exception in the child frame. This tests
+ * whether {@link Hub#unlatchStack} behaves appropriately for <b>CALL</b>-type and
+ * <b>CREATE</b>-type instructions, when either
  *
  * <ul>
  *   <li>the child frame finishes on an exception [yes/no]
  *   <li>the first instruction in the parent frame is exceptional
  * </ul>
  *
- * <p><b>Note.</b> All exceptions that we test are <b>stackUnderflowException</b>'s.</p>
+ * <p><b>Note.</b> All exceptions that we test are <b>stackUnderflowException</b>'s.
  */
 public class ExceptionAtContextReEntry {
 
-    /**
-     * {@link #firstInstructionAfterResumingFromUnsuccessfulMessageCallIsExceptional}
-     * tests what happens if both
-     *
-     * <ul>
-     *     <li> a STATICCALL ends in an exception
-     *     <li> the first instruction after the parent resumes execution is exceptional, too
-     * </ul>
-     *
-     * <b>Note.</b> Both exceptions are <b>stackUnderflowException</b>'s.
-     */
-    @Test
-    public void firstInstructionAfterResumingFromUnsuccessfulMessageCallIsExceptional() {
+  /**
+   * {@link #firstInstructionAfterResumingFromUnsuccessfulMessageCallIsExceptional} tests what
+   * happens if both
+   *
+   * <ul>
+   *   <li>a STATICCALL ends in an exception
+   *   <li>the first instruction after the parent resumes execution is exceptional, too
+   * </ul>
+   *
+   * <b>Note.</b> Both exceptions are <b>stackUnderflowException</b>'s.
+   */
+  @Test
+  public void firstInstructionAfterResumingFromUnsuccessfulMessageCallIsExceptional() {
 
-        BytecodeCompiler initCode = BytecodeCompiler.newProgram();
-        initCode
-                .push(ADD.byteValue())
-                .push(0)
-                .op(MSTORE8)
-                .push(1)
-                .push(0)
-                .op(RETURN);
-        // this init code deploys the byte code "0x01" (when given sufficient gas)
+    BytecodeCompiler initCode = BytecodeCompiler.newProgram();
+    initCode.push(ADD.byteValue()).push(0).op(MSTORE8).push(1).push(0).op(RETURN);
+    // this init code deploys the byte code "0x01" (when given sufficient gas)
 
-        BytecodeCompiler program = BytecodeCompiler.newProgram();
+    BytecodeCompiler program = BytecodeCompiler.newProgram();
 
-        // we do a deployment of a smart contract whose bytecode is "0x01"
-        Bytes initCodeBytes = initCode.compile();
+    // we do a deployment of a smart contract whose bytecode is "0x01"
+    Bytes initCodeBytes = initCode.compile();
 
-        program
-                .push(initCodeBytes)
-                        .push(0)
-                                .op(MSTORE);
-        program
-                .push(initCodeBytes.size())
-                .push(WORD_SIZE - initCodeBytes.size())
-                .push(0)
-                .op(CREATE);
-        // should leave the stack containing
-        // | deploymentAddress ]
+    program.push(initCodeBytes).push(0).op(MSTORE);
+    program.push(initCodeBytes.size()).push(WORD_SIZE - initCodeBytes.size()).push(0).op(CREATE);
+    // should leave the stack containing
+    // | deploymentAddress ]
 
-        final int storageKey = 255;
-        program
-                .push(storageKey)
-                        .op(SSTORE);
+    final int storageKey = 255;
+    program.push(storageKey).op(SSTORE);
 
-        // message call to a simple smart contract
-        program
-                .push(0) // r@c
-                .push(0) // r@c
-                .push(0) // r@c
-                .push(0) // r@c
-                .push(storageKey)
-                .op(SLOAD) // deployment address
-                .push(0xff) // gas
-                .op(STATICCALL);
+    // message call to a simple smart contract
+    program
+        .push(0) // r@c
+        .push(0) // r@c
+        .push(0) // r@c
+        .push(0) // r@c
+        .push(storageKey)
+        .op(SLOAD) // deployment address
+        .push(0xff) // gas
+        .op(STATICCALL);
 
-        // the previous operation leaves the stack in the following state
-        // | b ], where b ∈ {0, 1}, here: 0
-        // i.e. containing precisely one (boolean) item. The ADD instruction will therefore raise
-        // a stackUnderflowException.
-        program.op(ADD);
+    // the previous operation leaves the stack in the following state
+    // | b ], where b ∈ {0, 1}, here: 0
+    // i.e. containing precisely one (boolean) item. The ADD instruction will therefore raise
+    // a stackUnderflowException.
+    program.op(ADD);
 
-        BytecodeRunner.of(program.compile()).run();
-    }
+    BytecodeRunner.of(program.compile()).run();
+  }
 
-    /**
-     * {@link #firstInstructionAfterResumingFromSuccessfulMessageCallIsExceptional}
-     * tests what happens if both
-     *
-     * <ul>
-     *     <li> a STATICCALL ends in an exception
-     *     <li> the first instruction after the parent resumes execution is exceptional, too
-     * </ul>
-     *
-     * <b>Note.</b> Both exceptions are <b>stackUnderflowException</b>'s.
-     */
-    @Test
-    public void firstInstructionAfterResumingFromSuccessfulMessageCallIsExceptional() {
+  /**
+   * {@link #firstInstructionAfterResumingFromSuccessfulMessageCallIsExceptional} tests what happens
+   * if both
+   *
+   * <ul>
+   *   <li>a STATICCALL ends in an exception
+   *   <li>the first instruction after the parent resumes execution is exceptional, too
+   * </ul>
+   *
+   * <b>Note.</b> Both exceptions are <b>stackUnderflowException</b>'s.
+   */
+  @Test
+  public void firstInstructionAfterResumingFromSuccessfulMessageCallIsExceptional() {
 
-        BytecodeCompiler program = BytecodeCompiler.newProgram();
+    BytecodeCompiler program = BytecodeCompiler.newProgram();
 
-        // message call to a simple smart contract
-        program
-                .push(0) // r@c
-                .push(0) // r@c
-                .push(0) // r@c
-                .push(0) // r@c
-                .push("c0de") // address
-                .push(0xff) // gas
-                .op(STATICCALL);
+    // message call to a simple smart contract
+    program
+        .push(0) // r@c
+        .push(0) // r@c
+        .push(0) // r@c
+        .push(0) // r@c
+        .push("c0de") // address
+        .push(0xff) // gas
+        .op(STATICCALL);
 
-        // the previous operation leaves the stack in the following state
-        // | b ], where b ∈ {0, 1}
-        // i.e. containing precisely one (boolean) item. The ADD instruction will therefore raise
-        // a stackUnderflowException.
-        program.op(ADD);
+    // the previous operation leaves the stack in the following state
+    // | b ], where b ∈ {0, 1}
+    // i.e. containing precisely one (boolean) item. The ADD instruction will therefore raise
+    // a stackUnderflowException.
+    program.op(ADD);
 
-        BytecodeRunner.of(program.compile()).run();
-    }
+    BytecodeRunner.of(program.compile()).run();
+  }
 
-    /**
-     * {@link #firstInstructionAfterResumingFromSuccessfulContractCreationIsExceptional}
-     * tests what happens if the first instruction after a successful deployment is
-     * exceptional.
-     *
-     * <p><b>Note.</b> The exceptions is a <b>stackUnderflowException</b>.
-     */
-    @Test
-    public void firstInstructionAfterResumingFromSuccessfulContractCreationIsExceptional() {
+  /**
+   * {@link #firstInstructionAfterResumingFromSuccessfulContractCreationIsExceptional} tests what
+   * happens if the first instruction after a successful deployment is exceptional.
+   *
+   * <p><b>Note.</b> The exceptions is a <b>stackUnderflowException</b>.
+   */
+  @Test
+  public void firstInstructionAfterResumingFromSuccessfulContractCreationIsExceptional() {
 
-        BytecodeCompiler program = BytecodeCompiler.newProgram();
-        program
-                .push(0)
-                .push(0)
-                .push(0)
-                .op(CREATE);
+    BytecodeCompiler program = BytecodeCompiler.newProgram();
+    program.push(0).push(0).push(0).op(CREATE);
 
-        // the previous operation leaves the stack in the following state
-        // | a ], where a is either 0 or the deployment address, in the present case: the deployment address
-        // i.e. containing precisely one item. The ADD instruction will therefore raise a stackUnderflowException.
-        program.op(ADD);
+    // the previous operation leaves the stack in the following state
+    // | a ], where a is either 0 or the deployment address, in the present case: the deployment
+    // address
+    // i.e. containing precisely one item. The ADD instruction will therefore raise a
+    // stackUnderflowException.
+    program.op(ADD);
 
-        BytecodeRunner.of(program.compile()).run();
-    }
+    BytecodeRunner.of(program.compile()).run();
+  }
 
-    /**
-     * {@link #firstInstructionAfterResumingFromUnsuccessfulContractCreationIsExceptional}
-     * tests what happens if the first instruction after an unsuccessful deployment is
-     * exceptional, too.
-     *
-     * <p><b>Note.</b> The exceptions is a <b>stackUnderflowException</b>.
-     */
-    @Test
-    public void firstInstructionAfterResumingFromUnsuccessfulContractCreationIsExceptional() {
+  /**
+   * {@link #firstInstructionAfterResumingFromUnsuccessfulContractCreationIsExceptional} tests what
+   * happens if the first instruction after an unsuccessful deployment is exceptional, too.
+   *
+   * <p><b>Note.</b> The exceptions is a <b>stackUnderflowException</b>.
+   */
+  @Test
+  public void firstInstructionAfterResumingFromUnsuccessfulContractCreationIsExceptional() {
 
-        BytecodeCompiler program = BytecodeCompiler.newProgram();
-        program
-                .push(ADD.byteValue())
-                .push(0)
-                .op(MSTORE8);
-        // memory = [01 00 00 ... [
+    BytecodeCompiler program = BytecodeCompiler.newProgram();
+    program.push(ADD.byteValue()).push(0).op(MSTORE8);
+    // memory = [01 00 00 ... [
 
-        program
-                .push(1)
-                .push(0)
-                .push(0)
-                .op(CREATE);
-        // the init code is "0x01", which will cause an exception in the CREATE
+    program.push(1).push(0).push(0).op(CREATE);
+    // the init code is "0x01", which will cause an exception in the CREATE
 
-        // the previous operation leaves the stack in the following state
-        // | a ], where a is either 0 or the deployment address, in the present case: ZERO
-        // i.e. containing precisely one item. The ADD instruction will therefore raise a stackUnderflowException.
-        program.op(ADD);
+    // the previous operation leaves the stack in the following state
+    // | a ], where a is either 0 or the deployment address, in the present case: ZERO
+    // i.e. containing precisely one item. The ADD instruction will therefore raise a
+    // stackUnderflowException.
+    program.op(ADD);
 
-        BytecodeRunner.of(program.compile()).run();
-    }
+    BytecodeRunner.of(program.compile()).run();
+  }
 }

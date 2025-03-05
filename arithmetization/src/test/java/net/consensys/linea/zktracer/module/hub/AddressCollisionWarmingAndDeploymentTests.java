8c1c5e6668fd18e89d84bfc77380386b02aafe54
F Bojarski <ceciestunepoubelle@protonmail.ch>
2025-03-05 12:44:34 +0100

    typo
    Signed-off-by: F Bojarski <ceciestunepoubelle@protonmail.ch>

--- arithmetization/src/test/java/net/consensys/linea/zktracer/module/hub/AddressCollisionWarmingAndDeploymentTests.java
+++ arithmetization/src/test/java/net/consensys/linea/zktracer/module/hub/AddressCollisionWarmingAndDeploymentTests.java
@@ -42,7 +42,7 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
-public class AddressColisionWarmingAndDeploymentTests {
+public class AddressCollisionWarmingAndDeploymentTests {
 
   // ** This aims to test skipping, warming, initialization and finalization section, whith
   // scenarii:

b05e6d1d30e6c48ff7590da7120b7a672ea9778b
F Bojarski <ceciestunepoubelle@protonmail.ch>
2025-03-13 18:06:24 +0100

    add TODO
    Signed-off-by: F Bojarski <ceciestunepoubelle@protonmail.ch>

--- arithmetization/src/main/java/net/consensys/linea/zktracer/module/limits/L2Block.java
+++ arithmetization/src/main/java/net/consensys/linea/zktracer/module/limits/L2Block.java
@@ -114,6 +114,10 @@ public class L2Block implements Module {
     // Counts the number of Keccak from tx RLPs, used both for both the signature verification and
     // the public input computation.
     keccak.updateTally(txDataSize);
+    // TODO: this is for the RLP of the unsigned transaction, not the signed one. This is an upper
+    // bond. Waiting for Besu to expose the method that enables to compute the RLP of the un signed
+    // tx
+    keccak.updateTally(txDataSize);
   }
 
   @Override

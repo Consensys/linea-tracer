c14514c7212e26eb4c93fabae5a9b7671ce60699
F Bojarski <ceciestunepoubelle@protonmail.ch>
2025-05-05 10:29:10 +0530

    ras
    Signed-off-by: F Bojarski <ceciestunepoubelle@protonmail.ch>

--- arithmetization/src/main/java/net/consensys/linea/zktracer/Fork.java
+++ arithmetization/src/main/java/net/consensys/linea/zktracer/Fork.java
@@ -32,21 +32,9 @@ public enum Fork {
     };
   }
 
-  /**
-   * Construct a fork instance from the name of a fork (e.g. "London", "Shanghai", etc). Observe
-   * that case does not matter here. Hence, "LONDON", "London", "london", "lonDon" are all suitable
-   * aliases for the LONDON instance.
-   *
-   * @param fork
-   * @return
-   */
-  public static Fork fromString(String fork) {
-    return Fork.valueOf(fork.toUpperCase());
-  }
-
   public static boolean isPostShanghai(Fork fork) {
     return switch (fork) {
-      case LONDON, PARIS -> false;
+      case LONDON -> false;
       case SHANGHAI, CANCUN, PRAGUE -> true;
     };
   }

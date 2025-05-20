6e7d600caeb202834abf173956ccfd22585e0fe4
amkCha <29160563+amkCha@users.noreply.github.com>
2025-05-20 11:40:04 +0200

    remove shanghai for now on blockchain ref tests

--- reference-tests/src/test/java/net/consensys/linea/BlockchainReferenceTestTools.java
+++ reference-tests/src/test/java/net/consensys/linea/BlockchainReferenceTestTools.java
@@ -63,7 +63,7 @@ public class BlockchainReferenceTestTools {
   private static final ReferenceTestProtocolSchedules REFERENCE_TEST_PROTOCOL_SCHEDULES =
       ReferenceTestProtocolSchedules.create();
 
-  private static final List<String> NETWORKS_TO_RUN = List.of("London", "Shanghai");
+  private static final List<String> NETWORKS_TO_RUN = List.of("London");
 
   public static final JsonTestParameters<?, ?> PARAMS =
       JsonTestParameters.create(BlockchainReferenceTestCaseSpec.class)

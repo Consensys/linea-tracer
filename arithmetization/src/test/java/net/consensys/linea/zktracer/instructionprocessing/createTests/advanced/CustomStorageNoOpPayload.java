21e4c1e21cae3ee4ca8c4950d940bc2139dc8abc
Olivier Bégassat <olivier.begassat.cours@gmail.com>
2025-05-26 19:54:21 +0200

    ras

---
+++ arithmetization/src/test/java/net/consensys/linea/zktracer/instructionprocessing/createTests/advanced/CustomStorageNoOpPayload.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright Consensys Software Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+package net.consensys.linea.zktracer.instructionprocessing.createTests.advanced;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+import net.consensys.linea.testing.generated.Factory;
+import org.apache.tuweni.bytes.Bytes;
+import org.web3j.abi.FunctionEncoder;
+import org.web3j.abi.datatypes.Function;
+
+public class CustomStorageNoOpPayload {
+
+  public static Bytes deploy(String salt) {
+    Function function =
+        new Function(
+            Factory.FUNC_DEPLOY,
+            Arrays.asList(
+                new org.web3j.abi.datatypes.generated.Bytes32(
+                    Bytes.fromHexStringLenient(salt).toArray())),
+            Collections.emptyList());
+    return Bytes.fromHexStringLenient(FunctionEncoder.encode(function));
+  }
+
+  public static Bytes callMain(
+      Boolean _touchStorage, Boolean _modifyStorage, Boolean _selfdestruct, Boolean _revert) {
+    Function function =
+        new Function(
+            Factory.FUNC_CALLMAIN,
+            Arrays.asList(
+                new org.web3j.abi.datatypes.Bool(_touchStorage),
+                new org.web3j.abi.datatypes.Bool(_modifyStorage),
+                new org.web3j.abi.datatypes.Bool(_selfdestruct),
+                new org.web3j.abi.datatypes.Bool(_revert)),
+            Collections.emptyList());
+    return Bytes.fromHexStringLenient(FunctionEncoder.encode(function));
+  }
+}

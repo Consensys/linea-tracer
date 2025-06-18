eed2ecdf151320ff0b18acb521e08956b8111e4c
amkCha <29160563+amkCha@users.noreply.github.com>
2025-06-18 23:11:22 +0200

    spotless fix

--- arithmetization/src/test/java/net/consensys/linea/zktracer/module/limits/precompileLimits/ModexpIllegalOperationTests.java
+++ arithmetization/src/test/java/net/consensys/linea/zktracer/module/limits/precompileLimits/ModexpIllegalOperationTests.java
@@ -16,11 +16,9 @@
 package net.consensys.linea.zktracer.module.limits.precompileLimits;
 
 import static java.lang.Integer.MAX_VALUE;
-import static net.consensys.linea.zktracer.ChainConfig.MAINNET_LONDON_TESTCONFIG;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 import net.consensys.linea.reporting.TracerTestBase;
-import net.consensys.linea.zktracer.Fork;
 import net.consensys.linea.zktracer.ZkTracer;
 import net.consensys.linea.zktracer.module.limits.precompiles.ModexpEffectiveCall;
 import org.junit.jupiter.api.Test;

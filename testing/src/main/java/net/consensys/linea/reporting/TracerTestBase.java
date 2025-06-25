a1167f3f3480504486873d1ba1621a658dd7600c
amkCha <29160563+amkCha@users.noreply.github.com>
2025-06-25 14:19:01 +0200

    comment and remove logs

--- testing/src/main/java/net/consensys/linea/reporting/TracerTestBase.java
+++ testing/src/main/java/net/consensys/linea/reporting/TracerTestBase.java
@@ -15,9 +15,7 @@
 package net.consensys.linea.reporting;
 
 import net.consensys.linea.zktracer.ChainConfig;
-import net.consensys.linea.zktracer.Fork;
 import org.hyperledger.besu.datatypes.Address;
-import org.junit.jupiter.api.Assumptions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.TestInfo;
 

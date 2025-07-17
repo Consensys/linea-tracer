63347a9026c2a8843e118c98479ccb647bb3dd1b
amkCha <29160563+amkCha@users.noreply.github.com>
2025-07-17 15:27:05 +0200

    feat: add multi excep for transients and fix TStore section

--- arithmetization/src/test/java/net/consensys/linea/zktracer/exceptions/OutOfGasExceptionTest.java
+++ arithmetization/src/test/java/net/consensys/linea/zktracer/exceptions/OutOfGasExceptionTest.java
@@ -38,6 +38,7 @@ import net.consensys.linea.zktracer.opcode.OpCodes;
 import org.apache.tuweni.bytes.Bytes;
 import org.hyperledger.besu.datatypes.Address;
 import org.hyperledger.besu.datatypes.Wei;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
@@ -287,6 +288,7 @@ public class OutOfGasExceptionTest extends TracerTestBase {
   }
 
   /** We provide a non-zero key and value to store in transient storage to avoid trivialities */
+  @Tag("cancun")
   @ParameterizedTest
   @ValueSource(ints = {-1, 0, 1})
   void outOfGasExceptionTStore(int cornerCase) {
@@ -329,6 +331,30 @@ public class OutOfGasExceptionTest extends TracerTestBase {
       // TLOAD/TSTORE are not supported prior to Cancun fork
       return;
     }
+
+    Bytes pgCompile = program.compile();
+    BytecodeRunner bytecodeRunner = BytecodeRunner.of(pgCompile);
+
+    long gasCost = bytecodeRunner.runOnlyForGasCost(testInfo);
+
+    bytecodeRunner.run(gasCost + cornerCase, testInfo);
+
+    ExceptionUtils.assertEqualsOutOfGasIfCornerCaseMinusOneElseAssertNotEquals(
+        cornerCase, bytecodeRunner);
+  }
+
+  @Tag("cancun")
+  @ParameterizedTest
+  @ValueSource(ints = {-1, 0, 1})
+  void outOfGasExceptionTLoad(int cornerCase) {
+    BytecodeCompiler program = BytecodeCompiler.newProgram(testInfo);
+
+    program
+        .push(2) // value
+        .push(1) // key
+        .op(OpCode.TSTORE)
+        .push(1) // key
+        .op(OpCode.TLOAD);
 
     Bytes pgCompile = program.compile();
     BytecodeRunner bytecodeRunner = BytecodeRunner.of(pgCompile);

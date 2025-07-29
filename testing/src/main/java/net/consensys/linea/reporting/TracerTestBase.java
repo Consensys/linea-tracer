df687e48e177dea6791c53dd93c5e7e0b753ecc8
F Bojarski <ceciestunepoubelle@protonmail.ch>
2025-07-29 15:16:09 +0530

    same as before
    Signed-off-by: F Bojarski <ceciestunepoubelle@protonmail.ch>

--- testing/src/main/java/net/consensys/linea/reporting/TracerTestBase.java
+++ testing/src/main/java/net/consensys/linea/reporting/TracerTestBase.java
@@ -30,7 +30,7 @@ public class TracerTestBase {
   @BeforeEach
   public void init(TestInfo testInfo) {
       TracerTestBase.testInfo.chainConfig =
-              switch (getForkOrDefault("CANCUN")) {
+              switch (getForkOrDefault("LONDON")) {
               case "LONDON" -> ChainConfig.MAINNET_TESTCONFIG(LONDON);
               case "PARIS" -> ChainConfig.MAINNET_TESTCONFIG(PARIS);
               case "SHANGHAI" -> ChainConfig.MAINNET_TESTCONFIG(SHANGHAI);

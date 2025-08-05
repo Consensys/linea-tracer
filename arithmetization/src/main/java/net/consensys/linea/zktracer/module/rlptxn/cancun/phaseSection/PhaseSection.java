b0cc66a0127e51766d8795de72051832fb9710f2
F Bojarski <ceciestunepoubelle@protonmail.ch>
2025-08-05 17:54:38 +0530

    update constraints
    Signed-off-by: F Bojarski <ceciestunepoubelle@protonmail.ch>

--- arithmetization/src/main/java/net/consensys/linea/zktracer/module/rlptxn/cancun/phaseSection/PhaseSection.java
+++ arithmetization/src/main/java/net/consensys/linea/zktracer/module/rlptxn/cancun/phaseSection/PhaseSection.java
@@ -68,7 +68,6 @@ public abstract class PhaseSection {
   public void tracePreValues(Trace.Rlptxn trace, GenericTracedValue tracedValues) {
     traceIsPhaseX(trace);
     trace
-        .userTxnNumber(tracedValues.tx().getUserTransactionNumber())
         .indexLt(tracedValues.indexLt())
         .indexLx(tracedValues.indexLx())
         .codeFragmentIndex(tracedValues.tx().getCodeFragmentIndex())

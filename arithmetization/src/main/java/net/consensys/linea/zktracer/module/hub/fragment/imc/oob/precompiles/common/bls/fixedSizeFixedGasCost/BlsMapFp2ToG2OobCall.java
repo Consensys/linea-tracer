3b9c8eef8f44fca86d3a60629cd606080608d0fc
Lorenzo Gentile <lorenzo.gentile@consensys.net>
2025-08-06 16:08:08 +0200

    fixed constants usage

--- arithmetization/src/main/java/net/consensys/linea/zktracer/module/hub/fragment/imc/oob/precompiles/common/bls/fixedSizeFixedGasCost/BlsMapFp2ToG2OobCall.java
+++ arithmetization/src/main/java/net/consensys/linea/zktracer/module/hub/fragment/imc/oob/precompiles/common/bls/fixedSizeFixedGasCost/BlsMapFp2ToG2OobCall.java
@@ -15,10 +15,10 @@
 
 package net.consensys.linea.zktracer.module.hub.fragment.imc.oob.precompiles.common.bls.fixedSizeFixedGasCost;
 
-import static net.consensys.linea.zktracer.Trace.GAS_CONST_POINT_EVALUATION;
-import static net.consensys.linea.zktracer.Trace.OOB_INST_POINT_EVALUATION;
-import static net.consensys.linea.zktracer.Trace.PRC_POINT_EVALUATION_SIZE;
-import static net.consensys.linea.zktracer.TraceCancun.Oob.CT_MAX_POINT_EVALUATION;
+import static net.consensys.linea.zktracer.Trace.GAS_CONST_BLS_MAP_FP2_TO_G2;
+import static net.consensys.linea.zktracer.Trace.OOB_INST_BLS_MAP_FP2_TO_G2;
+import static net.consensys.linea.zktracer.Trace.PRC_BLS_MAP_FP2_TO_G2_SIZE;
+import static net.consensys.linea.zktracer.TraceCancun.Oob.CT_MAX_BLS_MAP_FP2_TO_G2;
 
 import java.math.BigInteger;
 
@@ -31,26 +31,26 @@ public class BlsMapFp2ToG2OobCall extends BlsFixedSizeFixedGasCostOobCall {
 
   @Override
   long precompileExpectedCds() {
-    return PRC_POINT_EVALUATION_SIZE;
+    return PRC_BLS_MAP_FP2_TO_G2_SIZE;
   }
 
   @Override
   long precompileLongCost() {
-    return GAS_CONST_POINT_EVALUATION;
+    return GAS_CONST_BLS_MAP_FP2_TO_G2;
   }
 
   @Override
   protected void traceOobInstructionInOob(Trace.Oob trace) {
-    trace.isPointEvaluation(true).oobInst(OOB_INST_POINT_EVALUATION);
+    trace.isPointEvaluation(true).oobInst(OOB_INST_BLS_MAP_FP2_TO_G2);
   }
 
   @Override
   protected void traceOobInstructionInHub(Trace.Hub trace) {
-    trace.pMiscOobInst(OOB_INST_POINT_EVALUATION);
+    trace.pMiscOobInst(OOB_INST_BLS_MAP_FP2_TO_G2);
   }
 
   @Override
   public int ctMax() {
-    return CT_MAX_POINT_EVALUATION;
+    return CT_MAX_BLS_MAP_FP2_TO_G2;
   }
 }

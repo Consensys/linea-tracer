37cd574fdaee395808e80cf10ea2b08c90cadb6e
amkCha <29160563+amkCha@users.noreply.github.com>
2025-08-18 14:28:44 +0200

    cn number into bytes mxp

--- arithmetization/src/test/java/net/consensys/linea/zktracer/exceptions/OutOfGasExceptionTest.java
+++ arithmetization/src/test/java/net/consensys/linea/zktracer/exceptions/OutOfGasExceptionTest.java
@@ -50,12 +50,6 @@ public class OutOfGasExceptionTest extends TracerTestBase {
   @ParameterizedTest
   @MethodSource("outOfGasExceptionWithEmptyAccountsAndNoMemoryExpansionCostTestSource")
   void outOfGasExceptionWithEmptyAccountsAndNoMemoryExpansionCostTest(int opcode, int cornerCase) {
-    // TODO: enable SELFDESTRUCT for Cancun again after fixing the issue
-    // https://github.com/Consensys/linea-tracer/issues/2159
-    // PR https://github.com/Consensys/linea-tracer/pull/2184
-    if (fork == Fork.CANCUN && opcode == 255) {
-      return;
-    }
     // Extract relevant opcode data
     OpCodeData opCodeData = opcodes.of(opcode);
     // Only test opcodes which do not cause memory expansion.

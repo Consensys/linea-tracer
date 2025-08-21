b80426011257a079250038cc3f7c959d5cd72c7d
amkCha <29160563+amkCha@users.noreply.github.com>
2025-08-21 15:25:19 +0200

    feat: add hack for trim && activate blockchain ref test for Cancun

--- arithmetization/src/main/java/net/consensys/linea/zktracer/module/hub/section/skip/TxSkipSection.java
+++ arithmetization/src/main/java/net/consensys/linea/zktracer/module/hub/section/skip/TxSkipSection.java
@@ -30,6 +30,7 @@ import net.consensys.linea.zktracer.module.hub.fragment.account.AccountFragment;
 import net.consensys.linea.zktracer.module.hub.section.TraceSection;
 import net.consensys.linea.zktracer.module.hub.transients.Transients;
 import net.consensys.linea.zktracer.types.TransactionProcessingMetadata;
+import org.hyperledger.besu.datatypes.AccessListEntry;
 import org.hyperledger.besu.datatypes.Address;
 import org.hyperledger.besu.datatypes.Transaction;
 import org.hyperledger.besu.datatypes.Wei;
@@ -148,6 +149,19 @@ public abstract class TxSkipSection extends TraceSection implements EndTransacti
     if (txMetadata.coinbaseAddressCollision()) {
       coinbase = coinbaseNew.deepCopy().decrementBalanceBy(txMetadata.getCoinbaseReward());
     }
+
+    txMetadata
+        .getBesuTransaction()
+        .getAccessList()
+        .ifPresent(
+            accessList -> {
+              if (!accessList.isEmpty()) {
+                for (AccessListEntry entry : accessList) {
+                  final Address address = entry.address();
+                  hub.trm().callTrimming(address);
+                }
+              }
+            });
 
     // "sender" account fragment
     final AccountFragment senderAccountFragment =

9c41a8db6bc198ae6b3fbab63dfd444edc7fcfe7
amkCha <29160563+amkCha@users.noreply.github.com>
2025-08-25 15:27:38 +0200

    fix: resB

--- arithmetization/src/main/java/net/consensys/linea/zktracer/module/mxp/moduleOperation/CancunMxpOperation.java
+++ arithmetization/src/main/java/net/consensys/linea/zktracer/module/mxp/moduleOperation/CancunMxpOperation.java
@@ -16,7 +16,6 @@
 package net.consensys.linea.zktracer.module.mxp.moduleOperation;
 
 import static net.consensys.linea.zktracer.module.mxp.MxpUtils.*;
-import static net.consensys.linea.zktracer.types.Conversions.bytesToLong;
 
 import lombok.Getter;
 import net.consensys.linea.zktracer.Trace;
@@ -139,7 +138,7 @@ public class CancunMxpOperation extends MxpOperation {
           .pComputationArg2Hi(cancunMxpCall.exoCalls[i].arg2Hi())
           .pComputationArg2Lo(cancunMxpCall.exoCalls[i].arg2Lo())
           .pComputationResA(cancunMxpCall.exoCalls[i].resultA())
-          .pComputationResB(bytesToLong(cancunMxpCall.exoCalls[i].resultB()))
+          .pComputationResB(cancunMxpCall.exoCalls[i].resultB())
           .fillAndValidateRow();
     }
   }

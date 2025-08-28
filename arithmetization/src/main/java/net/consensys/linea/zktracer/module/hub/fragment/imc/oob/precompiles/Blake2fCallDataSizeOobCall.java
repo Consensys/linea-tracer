e34c52f9fa59ae6458c5b26ab78904924396d88d
F Bojarski <ceciestunepoubelle@protonmail.ch>
2025-08-28 13:44:09 +0530

    feat: no duplicate for blake oob call
    Signed-off-by: F Bojarski <ceciestunepoubelle@protonmail.ch>

--- arithmetization/src/main/java/net/consensys/linea/zktracer/module/hub/fragment/imc/oob/precompiles/Blake2fCallDataSizeOobCall.java
+++ arithmetization/src/main/java/net/consensys/linea/zktracer/module/hub/fragment/imc/oob/precompiles/Blake2fCallDataSizeOobCall.java
@@ -21,6 +21,7 @@ import static net.consensys.linea.zktracer.module.oob.OobExoCall.callToEQ;
 import static net.consensys.linea.zktracer.module.oob.OobExoCall.callToIsZero;
 import static net.consensys.linea.zktracer.types.Conversions.*;
 
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 import net.consensys.linea.zktracer.Trace;
@@ -37,9 +38,13 @@ import org.hyperledger.besu.evm.frame.MessageFrame;
 
 @Getter
 @Setter
+@EqualsAndHashCode(onlyExplicitlyIncluded = true, callSuper = false)
 public class Blake2fCallDataSizeOobCall extends OobCall {
-  EWord cds;
-  EWord returnAtCapacity;
+  // Inputs
+  @EqualsAndHashCode.Include EWord cds;
+  @EqualsAndHashCode.Include EWord returnAtCapacity;
+
+  // Outputs
   boolean hubSuccess;
   boolean returnAtCapacityNonZero;
 
--- arithmetization/src/main/java/net/consensys/linea/zktracer/module/hub/fragment/imc/oob/precompiles/Blake2fParamsOobCall.java
+++ arithmetization/src/main/java/net/consensys/linea/zktracer/module/hub/fragment/imc/oob/precompiles/Blake2fParamsOobCall.java
@@ -25,6 +25,7 @@ import static org.hyperledger.besu.evm.internal.Words.clampedToLong;
 
 import java.math.BigInteger;
 
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 import net.consensys.linea.zktracer.Trace;
@@ -40,12 +41,15 @@ import org.hyperledger.besu.evm.frame.MessageFrame;
 
 @Getter
 @Setter
+@EqualsAndHashCode(onlyExplicitlyIncluded = true, callSuper = false)
 public class Blake2fParamsOobCall extends OobCall {
 
-  BigInteger calleeGas;
-  BigInteger blakeR;
-  BigInteger blakeF;
+  // Inputs
+  @EqualsAndHashCode.Include final BigInteger calleeGas;
+  @EqualsAndHashCode.Include BigInteger blakeR;
+  @EqualsAndHashCode.Include BigInteger blakeF;
 
+  // Outputs
   boolean ramSuccess;
   BigInteger returnGas;
 

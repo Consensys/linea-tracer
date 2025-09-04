6a5c6aa987b6f46abafec3293e3aced63fe5fd5f
DavePearce <dave01001110@gmail.com>
2025-09-05 10:40:01 +1200

    enable tracefile naming from (processed) TestInfo
    This puts in place code necessary for processing the TestInfo
    information generated with a given test.  However, this is a fairly
    significant and outstanding issue, namely that the static field within
    the TracerTestBase class cannot be relied upon in a concurrent setting.

--- testing/src/main/java/net/consensys/linea/testing/ExecutionEnvironment.java
+++ testing/src/main/java/net/consensys/linea/testing/ExecutionEnvironment.java
@@ -21,6 +21,8 @@ import static net.consensys.linea.zktracer.Trace.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.io.IOException;
+import java.lang.reflect.Method;
+import java.lang.reflect.Parameter;
 import java.math.BigInteger;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -107,10 +109,7 @@ public class ExecutionEnvironment {
       long endBlock,
       TestInfo testInfo) {
     try {
-      if(testInfo != null) {
-        System.out.println("TEST NAME: " + testInfo.getTestMethod().get().getName() + testInfo.getDisplayName());
-      }
-      String prefix = constructTestPrefix(zkTracer.getChain());
+      String prefix = constructTestPrefix(zkTracer.getChain(), testInfo);
       Path traceFilePath = Files.createTempFile(prefix, ".lt");
       zkTracer.writeToFile(traceFilePath, startBlock, endBlock);
       final Path finalTraceFilePath = traceFilePath;
@@ -206,6 +205,93 @@ public class ExecutionEnvironment {
   private static final String LINEA_PACKAGE = "net.consensys.linea.";
 
   /**
+   * Construct a suitable prefix for the temporary lt file generated based on details (such as the name) of the
+   * test in question.  If a TestInfo instance is provided, this will be preferred.  If not, then it will fallback on
+   * a stack walking algorithm which attempts to figure out a suitable prefix (which can fail, and never includes
+   * parametric test arguments).
+   *
+   * @return
+   */
+  public static String constructTestPrefix(ChainConfig chain, TestInfo testInfo) {
+    String fork = chain.fork.toString();
+    // Check whether we have suitable testInfo information.
+    if(testInfo == null || testInfo.getTestMethod().isEmpty() || testInfo.getTestClass().isEmpty()) {
+      // No, therefore fall back on legacy mechanism.
+      return constructLegacyTestPrefix(fork);
+    }
+    // Extract key information
+    String className = testInfo.getTestClass().get().getSimpleName();
+    Method method = testInfo.getTestMethod().get();
+    String methodName = testInfo.getTestMethod().get().getName();
+    String params = processTestName(method, testInfo.getDisplayName());
+    // Done (for now)
+    return className + "_" + methodName + "_" + params + fork.toLowerCase() + "_";
+  }
+
+  /**
+   * This method attempts to process the given test name into a more human-readable form.  In particular, for
+   * parameterised tests, we want to change things like "0x000001" into just "0x1", etc.
+   * @param methodName name of the enclosing method.
+   * @param displayName provided display name to be processed.
+   * @return
+   */
+  private static String processTestName(Method method, String displayName) {
+    String[] values, split;
+    StringBuilder builder = new StringBuilder();
+    // remove method name if it is embedded
+    displayName = displayName.replace(method.getName(),"");
+    // remove any commas
+    displayName = displayName.replace(",","");
+    // split out any parameters
+    split = displayName.split(" ");
+    //
+    for (int i=0;i<split.length;i++) {
+      String val = split[i];
+      Parameter param;
+      // Skip test index, as not super helpful.
+      if(val.startsWith("[")) {
+        continue;
+      }
+      //
+      builder.append(processTestArgument(val));
+      builder.append("_");
+    }
+    // Limit maximum length of string to ensure the filename is not too long.
+    String result = builder.toString();
+    return result.substring(0,Math.min(100,result.length()));
+  }
+
+  private static String processTestArgument(String arg) {
+    if(arg.isEmpty()) {
+      return arg;
+    } else if(Character.isDigit(arg.charAt(0))) {
+      // Looks like a number
+      return processTestNumericArgument(arg);
+    } else {
+      // default
+      return arg;
+    }
+  }
+
+  private static String processTestNumericArgument(String arg) {
+    if(arg.startsWith("0x")) {
+      // hex
+      arg = arg.substring(2);
+      arg = arg.replaceFirst("^0*", "");
+      if(arg.isEmpty()) {
+        return "0x0";
+      }
+      return "0x" + arg;
+    } else {
+      arg = arg.replaceFirst("^0*", "");
+      if(arg.isEmpty()) {
+        return "0";
+      }
+      return arg;
+    }
+  }
+
+  /**
    * Construct a suitable prefix for the temporary lt file generated based on the method name of the
    * test. This is done by walking up the stack looking for a calling method whose classname ends
    * with "Test". Having found such a method, its name is then used as the test prefix. If no method
@@ -213,9 +299,7 @@ public class ExecutionEnvironment {
    *
    * @return
    */
-  public static String constructTestPrefix(ChainConfig chain) {
-    String fork = chain.fork.toString();
-    //
+  public static String constructLegacyTestPrefix(String fork) {
     for (StackTraceElement ste : Thread.currentThread().getStackTrace()) {
       String className = ste.getClassName();
       if (className.endsWith("Test") || className.endsWith("Tests")) {

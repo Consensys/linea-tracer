82ab2d3aa1611f81777ee9510f137e19f54473e4
Lorenzo Gentile <lorenzo.gentile@consensys.net>
2025-10-03 15:04:53 +0200

    fixed typo

--- arithmetization/src/test/java/net/consensys/linea/zktracer/module/mxp/MxpTest.java
+++ arithmetization/src/test/java/net/consensys/linea/zktracer/module/mxp/MxpTest.java
@@ -372,7 +372,7 @@ public class MxpTest extends TracerTestBase {
   static final BigInteger CANCUN_MXPX_THRESHOLD =
       (BigInteger.valueOf(256).pow(4)).subtract(BigInteger.ONE);
   static final BigInteger LONDON_MXPX_THRESHOLD_DIVIDED_BY_TWO =
-      (BigInteger.valueOf(256).pow(4)).divide(BigInteger.ZERO);
+      (BigInteger.valueOf(256).pow(4)).divide(BigInteger.TWO);
   static final BigInteger SMALL = BigInteger.valueOf(32);
 
   static Stream<Arguments> testMxpxThresholdSource() {

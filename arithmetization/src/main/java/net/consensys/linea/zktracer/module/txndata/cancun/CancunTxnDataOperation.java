eece27e91029ba2ae575893087f5f1315909aea1
DavePearce <dave01001110@gmail.com>
2025-10-21 15:42:26 +1300

    cherry-pick commit 3edbc80
    This cherry-picks a commit made against `beta-v4.0-rc17` entitled
    simply `ras` which updated the calculation of `relativeUserTxNumMax`.
    
    This also then updates `linea-constraints` accordingly to ensure all is
    well.

--- arithmetization/src/main/java/net/consensys/linea/zktracer/module/txndata/cancun/CancunTxnDataOperation.java
+++ arithmetization/src/main/java/net/consensys/linea/zktracer/module/txndata/cancun/CancunTxnDataOperation.java
@@ -76,8 +76,7 @@ public abstract class CancunTxnDataOperation extends TxnDataOperation {
 
   private void traceCommonSaveForFlags(
       Trace.Txndata trace, int ct, long totalUserTransactionsInConflation) {
-    final long relativeUserTxNumMax =
-        this instanceof CancunUserTransaction ? blockSnapshot.getNbOfTxsInBlock() : 0;
+    final long relativeUserTxNumMax = blockSnapshot.getNbOfTxsInBlock();
     trace
         // BLK_NUMBER is (defcomputed ...)
         // TOTL_TXN_NUMBER is (defcomputed ...)

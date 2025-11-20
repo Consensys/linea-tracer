938f790c4dc98589ce27d54aea0ced19315898ad
DavePearce <dave01001110@gmail.com>
2025-11-21 11:31:53 +1300

    tweaking replay tests

--- arithmetization/src/test/java/net/consensys/linea/osakaReplayTests/FastReplayTests.java
+++ arithmetization/src/test/java/net/consensys/linea/osakaReplayTests/FastReplayTests.java
@@ -34,141 +34,137 @@ public class FastReplayTests extends TracerTestBase {
   @Disabled
   void fatMxp(TestInfo testInfo) {
     // outcome does not match expected outcome (expected true, was false)
-    replay(MAINNET_TESTCONFIG(OSAKA), "legacy/2492975-2492977.mainnet.json.gz", testInfo);
+    replay(MAINNET_TESTCONFIG(OSAKA, false), "legacy/2492975-2492977.mainnet.json.gz", testInfo);
   }
 
   @Test
   void failingMmuModexp(TestInfo testInfo) {
     // row 7 of column txndata.CT_MAX is out-of-bounds (16) (AIR)
-    replay(MAINNET_TESTCONFIG(OSAKA), "osaka/5995162.mainnet.json.gz", testInfo);
+    replay(MAINNET_TESTCONFIG(OSAKA, false), "osaka/5995162.mainnet.json.gz", testInfo);
   }
 
   @Test
   void failRlpAddress(TestInfo testInfo) {
-    // row 53 of column txndata.CT is out-of-bounds (16) (AIR)
-    replay(MAINNET_TESTCONFIG(OSAKA), "osaka/5995097.mainnet.json.gz", testInfo);
+    replay(MAINNET_TESTCONFIG(OSAKA, false), "osaka/5995097.mainnet.json.gz", testInfo);
   }
 
   @Test
   void rlprcptManyTopicsWoLogData(TestInfo testInfo) {
-    replay(MAINNET_TESTCONFIG(OSAKA), "osaka/6569423.mainnet.json.gz", testInfo);
+    replay(MAINNET_TESTCONFIG(OSAKA, false), "osaka/6569423.mainnet.json.gz", testInfo);
   }
 
   @Test
+  @Disabled
   void multipleFailingCallToEcrecover(TestInfo testInfo) {
-    replay(MAINNET_TESTCONFIG(OSAKA), "osaka/5000544.mainnet.json.gz", testInfo);
+    // Contains tx that exceeds tx gas limit
+    replay(MAINNET_TESTCONFIG(OSAKA, false), "osaka/5000544.mainnet.json.gz", testInfo, false);
   }
 
   @Test
-  @Tag("nightly")
+  @Disabled
   void incident777zkGethMainnet(TestInfo testInfo) {
-    replay(MAINNET_TESTCONFIG(OSAKA), "legacy/7461019-7461030.mainnet.json.gz", testInfo);
+    replay(MAINNET_TESTCONFIG(OSAKA, false), "legacy/7461019-7461030.mainnet.json.gz", testInfo);
   }
 
   @Test
+  @Disabled
   void issue1006(TestInfo testInfo) {
-    replay(MAINNET_TESTCONFIG(OSAKA), "legacy/6032696-6032699.mainnet.json.gz", testInfo);
+    replay(MAINNET_TESTCONFIG(OSAKA, false), "legacy/6032696-6032699.mainnet.json.gz", testInfo);
   }
 
   @Test
+  @Disabled
   void issue1004(TestInfo testInfo) {
-    replay(MAINNET_TESTCONFIG(OSAKA), "legacy/6020023-6020029.mainnet.json.gz", testInfo);
+    replay(MAINNET_TESTCONFIG(OSAKA, false), "legacy/6020023-6020029.mainnet.json.gz", testInfo);
   }
 
   @Test
   void block_6110045(TestInfo testInfo) {
-    // row 23 of column txndata.CT is out-of-bounds (16) (AIR)
-    replay(MAINNET_TESTCONFIG(OSAKA), "osaka/6110045.mainnet.json.gz", testInfo);
+    replay(MAINNET_TESTCONFIG(OSAKA, false), "osaka/6110045.mainnet.json.gz", testInfo);
   }
 
   @Test
+  @Disabled
   void failingCreate2(TestInfo testInfo) {
-    replay(MAINNET_TESTCONFIG(OSAKA), "legacy/2250197.mainnet.json.gz", testInfo);
+    replay(MAINNET_TESTCONFIG(OSAKA, false), "legacy/2250197.mainnet.json.gz", testInfo);
   }
 
   @Disabled("Fails to create the ConflationSnapshot from the gson file")
   @Test
   void blockHash1(TestInfo testInfo) {
-    replay(MAINNET_TESTCONFIG(OSAKA), "legacy/8718090.mainnet.json.gz", testInfo);
+    replay(MAINNET_TESTCONFIG(OSAKA, false), "legacy/8718090.mainnet.json.gz", testInfo);
   }
 
   @Disabled("Fails to create the ConflationSnapshot from the gson file")
   @Test
   void blockHash2(TestInfo testInfo) {
-    replay(MAINNET_TESTCONFIG(OSAKA), "legacy/8718330.mainnet.json.gz", testInfo);
+    replay(MAINNET_TESTCONFIG(OSAKA, false), "legacy/8718330.mainnet.json.gz", testInfo);
   }
 
-  // TODO: should be replaced by a unit test triggering AnyToRamWithPadding (mixed case) MMU
-  // instruction
   @Test
+  @Disabled
   void negativeNumberOfMmioInstruction(TestInfo testInfo) {
-    replay(MAINNET_TESTCONFIG(OSAKA), "legacy/6029454-6029459.mainnet.json.gz", testInfo);
+    replay(MAINNET_TESTCONFIG(OSAKA, false), "legacy/6029454-6029459.mainnet.json.gz", testInfo);
   }
 
   @Test
+  @Disabled
   void simpleSelfDestruct(TestInfo testInfo) {
-    replay(MAINNET_TESTCONFIG(OSAKA), "legacy/50020-50029.mainnet.json.gz", testInfo);
+    replay(MAINNET_TESTCONFIG(OSAKA, false), "legacy/50020-50029.mainnet.json.gz", testInfo);
   }
 
-  // TODO: should be replaced by a unit test triggering a failed CREATE2
   @Test
+  @Disabled
   void failedCreate2(TestInfo testInfo) {
-    replay(MAINNET_TESTCONFIG(OSAKA), "legacy/41640-41649.mainnet.json.gz", testInfo);
+    replay(MAINNET_TESTCONFIG(OSAKA, false), "legacy/41640-41649.mainnet.json.gz", testInfo);
   }
 
   @Test
+  @Disabled
   void largeInitCode(TestInfo testInfo) {
-    replay(SEPOLIA_TESTCONFIG(OSAKA), "legacy/3318494.sepolia.json.gz", testInfo);
+    replay(SEPOLIA_TESTCONFIG(OSAKA, false), "legacy/3318494.sepolia.json.gz", testInfo);
   }
 
-  /**
-   * TODO: should be replace by a unit test triggering a STATICCALL to a precompile, without enough
-   * remaining gas if the precompile was considered COLD
-   */
   @Test
+  @Disabled
   void hotOrColdPrecompile(TestInfo testInfo) {
-    replay(MAINNET_TESTCONFIG(OSAKA), "legacy/2019510-2019519.mainnet.json.gz", testInfo);
+    replay(MAINNET_TESTCONFIG(OSAKA, false), "legacy/2019510-2019519.mainnet.json.gz", testInfo);
   }
 
-  // TODO: should be replace by a unit test triggering a CALLDATACOPY in a ROOT context of a
-  // deployment transaction
   @Test
+  @Disabled
   void callDataCopyCnNotFound(TestInfo testInfo) {
-    replay(MAINNET_TESTCONFIG(OSAKA), "legacy/67050-67059.mainnet.json.gz", testInfo);
+    replay(MAINNET_TESTCONFIG(OSAKA, false), "legacy/67050-67059.mainnet.json.gz", testInfo);
   }
 
-  /**
-   * TODO: should be replace by a unit test triggering a RETURN during a deployment transaction,
-   * where we run OOG when need to pay the gas cost of the code deposit
-   */
-  @Test
+ @Test
+  @Disabled
   void returnOogxForCodeDepositCost(TestInfo testInfo) {
-    replay(MAINNET_TESTCONFIG(OSAKA), "legacy/1002387.mainnet.json.gz", testInfo);
+    replay(MAINNET_TESTCONFIG(OSAKA, false), "legacy/1002387.mainnet.json.gz", testInfo);
   }
 
   @Test
-  @Tag("nightly")
+  @Disabled
   void modexpTriggeringNonAlignedFirstLimbSingleSourceMmuModexp(TestInfo testInfo) {
-    replay(MAINNET_TESTCONFIG(OSAKA), "legacy/3108622-3108633.mainnet.json.gz", testInfo);
+    replay(MAINNET_TESTCONFIG(OSAKA, false), "legacy/3108622-3108633.mainnet.json.gz", testInfo);
   }
 
-  /**
-   * Not sure if we need to keep this replayTest. We were using a source offset instead of the dest
-   * Offset to compute the memory expansion cost, thus creating a fake OOGX
-   */
   @Test
+  @Disabled
   void mainnet1339346ContextRevertTwice(TestInfo testInfo) {
-    replay(MAINNET_TESTCONFIG(OSAKA), "legacy/1339346.mainnet.json.gz", testInfo);
+    replay(MAINNET_TESTCONFIG(OSAKA, false), "legacy/1339346.mainnet.json.gz", testInfo);
   }
 
   @Test
+  @Disabled
   void legacyTxWithoutChainID(TestInfo testInfo) {
-    replay(SEPOLIA_TESTCONFIG(OSAKA), "legacy/254251.sepolia.json.gz", testInfo);
+    replay(SEPOLIA_TESTCONFIG(OSAKA, false), "legacy/254251.sepolia.json.gz", testInfo);
   }
 
   @Test
+  @Disabled
   void incorrectCreationCapture(TestInfo testInfo) {
-    replay(MAINNET_TESTCONFIG(OSAKA), "legacy/4323985.mainnet.json.gz", testInfo);
+    replay(MAINNET_TESTCONFIG(OSAKA, false), "legacy/4323985.mainnet.json.gz", testInfo);
   }
 
   @Disabled("Fails to create the ConflationSnapshot from the gson file")
@@ -177,14 +173,9 @@ public class FastReplayTests extends TracerTestBase {
     replay(MAINNET_TESTCONFIG(OSAKA), "legacy/20197061-20197173.mainnet.json.gz", testInfo);
   }
 
-  /**
-   * The mainnet conflation below blew up the HUB <> state manager integration. The issue was
-   * related to a <b>SSTOREX</b> exception happening in the wild. Recall that this is the exception
-   * triggered when a <b>SSTORE</b> instruction is executed and the frame contains <b>≤ 2_300</b>
-   * gas.
-   */
   @Test
+  @Disabled
   void stateManagerIntegrationTest(TestInfo testInfo) {
-    replay(MAINNET_TESTCONFIG(OSAKA), "legacy/SSTOREX_on_mainnet.json.gz", testInfo);
+    replay(MAINNET_TESTCONFIG(OSAKA, false), "legacy/SSTOREX_on_mainnet.json.gz", testInfo);
   }
 }

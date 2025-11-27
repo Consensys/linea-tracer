3d32478a25fa23f07d6c63e56656d5ec5661457e
Lorenzo Gentile <lorenzo.gentile@consensys.net>
2025-11-27 15:48:39 +0100

    spotless

--- arithmetization/src/test/java/net/consensys/linea/zktracer/precompiles/modexp/ModexpEIP7883Tests.java
+++ arithmetization/src/test/java/net/consensys/linea/zktracer/precompiles/modexp/ModexpEIP7883Tests.java
@@ -134,7 +134,7 @@ public class ModexpEIP7883Tests extends TracerTestBase {
         .op(OpCode.JUMPDEST, 32); // TODO: temporary workaround for go-corset issue
 
     // TODO: do we want to add a RETURNDATACOPY?
-    
+
     BytecodeRunner bytecodeRunner = BytecodeRunner.of(program.compile());
     bytecodeRunner.run(List.of(codeOwnerAccount), chainConfig, testInfo);
   }
